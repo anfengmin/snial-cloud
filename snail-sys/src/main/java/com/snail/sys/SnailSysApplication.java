49e5db21d6ceb4628a15441e35b4c38626020b12
anfengmin <anfengmin@yeah.net>
2025-05-09 15:40:34 +0800

    feat(service):

---
+++ snail-sys/src/main/java/com/snail/sys/SnailSysApplication.java
@@ -0,0 +1,16 @@
+package com.snail.sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 系统管理模块启动程序
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SnailSysApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SnailSysApplication.class, args);
+    }
+}
