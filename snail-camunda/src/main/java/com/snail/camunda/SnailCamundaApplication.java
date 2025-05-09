31b29dd43c0df933cd030e36ba65a5ca1ec1a6b8
anfengmin <anfengmin@yeah.net>
2025-05-09 16:41:52 +0800

    build(snail-camunda):移除请假流程相关代码
    - 删除了 CustomJacksonSerializer、JobExecutorConfiguration、LeaveProcessController、LeaveProcessEventListener、LeaveProcessService、LeaveProcessServiceImpl 和 LeaveRequest 类
    -清除了 application.yml 中与 Camunda 相关的配置
    -移除了 Dubbo 和 Nacos 的配置

--- snail-camunda/src/main/java/com/snail/camunda/SnailCamundaApplication.java
+++ snail-camunda/src/main/java/com/snail/camunda/SnailCamundaApplication.java
@@ -2,12 +2,11 @@ package com.snail.camunda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 流程服务模块启动程序
  */
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class SnailCamundaApplication {
     public static void main(String[] args) {
--- snail-sys/src/main/java/com/snail/sys/SnailSysApplication.java
+++ snail-sys/src/main/java/com/snail/sys/SnailSysApplication.java
@@ -2,12 +2,11 @@ package com.snail.sys;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 系统管理模块启动程序
  */
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class SnailSysApplication {
     public static void main(String[] args) {
