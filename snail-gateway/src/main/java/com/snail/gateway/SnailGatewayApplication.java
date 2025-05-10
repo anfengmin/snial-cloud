8056575efb1920fe0a51451dc07327bce46a0c2a
Levi <anfengmin@yeah.net>
2025-05-10 23:30:11 +0800

    feat(add):init

--- snail-gateway/src/main/java/com/snail/gateway/SnailGatewayApplication.java
+++ snail-gateway/src/main/java/com/snail/gateway/SnailGatewayApplication.java
@@ -2,15 +2,17 @@ package com.snail.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 网关启动程序
+ * @author ansir
  */
-@EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SnailGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(SnailGatewayApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  网关启动成功   ლ(´ڡ`ლ)ﾞ  ");
     }
 }
