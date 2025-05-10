9a4006a59d55923003e8c8cc29813016eb8e8ce8
Levi <anfengmin@yeah.net>
2025-05-10 19:48:45 +0800

    feat(core): 添加基础工具类和异常处理
    - 新增 AddressUtils、FileUtils、RegionUtils 工具类
    - 添加 ServiceException 异常类
    - 实现 IP地址解析、文件处理和区域定位功能

---
+++ snail-common/snail-common-core/src/main/java/common/core/utils/AddressUtils.java
@@ -0,0 +1,11 @@
+package common.core.utils;
+
+/**
+ * No explanation is needed
+ *
+ * @author Levi.
+ * Created time 2025/5/10
+ * @since 1.0
+ */
+public class AddressUtils {
+}
---
+++ snail-common/snail-common-core/src/main/java/common/core/utils/ip/AddressUtils.java
@@ -0,0 +1,44 @@
+package common.core.utils.ip;
+
+import cn.hutool.core.net.NetUtil;
+import cn.hutool.http.HtmlUtil;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * 获取地址类
+ *
+ * @author Levi.
+ * Created time 2025/5/10
+ * @since 1.0
+ */
+@Slf4j
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class AddressUtils {
+
+    /**
+     * 未知地址
+     */
+    public static final String UNKNOWN = "XX XX";
+
+    /**
+     * It's a magic method - Levi
+     *
+     * @param ip ip
+     * @return java.lang.String
+     * @since 1.0
+     */
+    public static String getRealAddressByIP(String ip) {
+        if (StringUtils.isBlank(ip)) {
+            return UNKNOWN;
+        }
+        // 内网不查询
+        ip = StringUtils.contains(ip, "0:0:0:0:0:0:0:1") ? "127.0.0.1" : HtmlUtil.cleanHtmlTag(ip);
+        if (NetUtil.isInnerIP(ip)) {
+            return "内网IP";
+        }
+        return RegionUtils.getCityInfo(ip);
+    }
+}
