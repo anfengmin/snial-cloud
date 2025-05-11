d256846beabf6c6e6173cab1338aadd115c384e2
Levi <anfengmin@yeah.net>
2025-05-11 16:59:28 +0800

    feat(add):init

---
+++ snail-sys/src/main/java/com/snail/auth/service/impl/SysLoginServiceImpl.java
@@ -0,0 +1,28 @@
+package com.snail.auth.service.impl;
+
+import com.snail.auth.service.SysLoginService;
+import org.springframework.stereotype.Service;
+
+/**
+ * No explanation is needed
+ *
+ * @author Levi.
+ * Created time 2025/5/11
+ * @since 1.0
+ */
+@Service
+public class SysLoginServiceImpl implements SysLoginService {
+
+    /**
+     * login
+     *
+     * @param username username
+     * @param password password
+     * @return java.lang.String
+     * @since 1.0
+     */
+    @Override
+    public String login(String username, String password) {
+        return null;
+    }
+}
