e2855ce04baadab49174a4b1d2121c70d96b2099
Levi <anfengmin@yeah.net>
2025-05-11 21:06:28 +0800

    feat(add):init

---
+++ snail-sys/src/main/java/com/snail/sys/domain/User.java
@@ -0,0 +1,88 @@
+package com.snail.sys.domain;
+
+import java.util.Date;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import common.core.domain.BaseEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 用户信息表(User)实体类
+ *
+ * @author makejava
+ * @since 2025-05-11 20:30:03
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@TableName("sys_user")
+@ApiModel(value = "用户信息表")
+public class User extends BaseEntity {
+
+    private static final long serialVersionUID = -79664786848105213L;
+
+
+    @TableId(type = IdType.AUTO)
+    @ApiModelProperty(value = "用户ID")
+    private Long id;
+
+    @ApiModelProperty(value = "部门ID")
+    private Long deptId;
+
+    @ApiModelProperty(value = "用户账号")
+    private String userName;
+
+    @ApiModelProperty(value = "用户昵称")
+    private String nickName;
+
+    @ApiModelProperty(value = "用户类型（sys_user系统用户）")
+    private String userType;
+
+    @ApiModelProperty(value = "用户邮箱")
+    private String email;
+
+    @ApiModelProperty(value = "手机号码")
+    private String phonenumber;
+
+    @ApiModelProperty(value = "用户性别（0男 1女 2未知）")
+    private String sex;
+
+    @ApiModelProperty(value = "头像地址")
+    private String avatar;
+
+    @ApiModelProperty(value = "密码")
+    private String password;
+
+    @ApiModelProperty(value = "帐号状态（0正常 1停用）")
+    private String status;
+
+    @ApiModelProperty(value = "删除标志（0代表存在 2代表删除）")
+    private String delFlag;
+
+    @ApiModelProperty(value = "最后登录IP")
+    private String loginIp;
+
+    @ApiModelProperty(value = "最后登录时间")
+    private Date loginDate;
+
+    @ApiModelProperty(value = "创建者")
+    private String createBy;
+
+    @ApiModelProperty(value = "创建时间")
+    private Date createTime;
+
+    @ApiModelProperty(value = "更新者")
+    private String updateBy;
+
+    @ApiModelProperty(value = "更新时间")
+    private Date updateTime;
+
+    @ApiModelProperty(value = "备注")
+    private String remark;
+
+}
