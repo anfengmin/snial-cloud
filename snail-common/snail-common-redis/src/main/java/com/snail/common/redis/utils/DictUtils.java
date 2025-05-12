a9f6eb2d3597381e22c0828682471255087419d4
anfengmin <anfengmin@yeah.net>
2025-05-12 11:37:22 +0800

    build(snail-common-bom): 更新项目依赖版本
    - 在 snail-common-bom 模块的 pom.xml 文件中，添加了核心模块和缓存服务的注释- 此修改提高了项目依赖管理的可读性和可维护性

---
+++ snail-common/snail-common-redis/src/main/java/com/snail/common/redis/utils/DictUtils.java
@@ -0,0 +1,51 @@
+package com.snail.common.redis.utils;
+
+import common.core.constant.CacheConstants;
+
+import java.util.List;
+
+/**
+ * 字典工具类
+ *
+ * @author Anfm
+ * Created time 2025/5/12
+ * @since 1.0
+ */
+public class DictUtils {
+
+//    /**
+//     * 设置字典缓存
+//     *
+//     * @param key       参数键
+//     * @param dictDatas 字典数据列表
+//     */
+//    public static void setDictCache(String key, List<SysDictData> dictDatas) {
+//        CacheUtils.put(CacheConstants.SYS_DICT_KEY, key, dictDatas);
+//    }
+//
+//    /**
+//     * 获取字典缓存
+//     *
+//     * @param key 参数键
+//     * @return dictDatas 字典数据列表
+//     */
+//    public static List<SysDictData> getDictCache(String key) {
+//        return CacheUtils.get(CacheConstants.SYS_DICT_KEY, key);
+//    }
+
+    /**
+     * 删除指定字典缓存
+     *
+     * @param key 字典键
+     */
+    public static void removeDictCache(String key) {
+        CacheUtils.evict(CacheConstants.SYS_DICT_KEY, key);
+    }
+
+    /**
+     * 清空字典缓存
+     */
+    public static void clearDictCache() {
+        CacheUtils.clear(CacheConstants.SYS_DICT_KEY);
+    }
+}
