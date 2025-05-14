722863c6e9fbce421b570ffe15e663bae514867d
anfengmin <anfengmin@yeah.net>
2025-05-14 17:06:31 +0800

    feat(snail-common-satoken): 实现 sa-token 权限管理
    - 新增 SaPermissionImpl 类实现 StpInterface 接口- 提供菜单权限和角色权限的获取方法
    - 利用 LoginUtils 获取当前登录用户信息

---
+++ snail-common/snail-common-satoken/src/main/java/com/snail/common/satoken/core/service/SaPermissionImpl.java
@@ -0,0 +1,38 @@
+package com.snail.common.satoken.core.service;
+
+import cn.dev33.satoken.stp.StpInterface;
+import com.snail.common.satoken.utils.LoginUtils;
+import com.snail.sys.api.vo.UserVo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * sa-token 权限管理实现类
+ *
+ * @author Anfm
+ * Created time 2025/5/14
+ * @since 1.0
+ */
+public class SaPermissionImpl implements StpInterface {
+
+    /**
+     * 获取菜单权限列表
+     */
+    @Override
+    public List<String> getPermissionList(Object loginId, String loginType) {
+        UserVo userVo = LoginUtils.getLoginUser();
+        assert userVo != null;
+        return new ArrayList<>(userVo.getMenuPermission());
+    }
+
+    /**
+     * 获取角色权限列表
+     */
+    @Override
+    public List<String> getRoleList(Object loginId, String loginType) {
+        UserVo userVo = LoginUtils.getLoginUser();
+        assert userVo != null;
+        return new ArrayList<>(userVo.getRolePermission());
+    }
+}
