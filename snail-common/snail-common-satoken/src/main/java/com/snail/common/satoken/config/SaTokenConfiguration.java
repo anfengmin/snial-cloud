abc575a2031dbd932784bd45fa98c3f80f28b77d
anfengmin <anfengmin@yeah.net>
2025-05-14 17:06:45 +0800

    feat(snail-common-satoken): 添加 Sa-Token 配置类- 新增 SaTokenConfiguration 类，配置 Sa-Token 相关组件- 注入 StpLogicJwtFor

---
+++ snail-common/snail-common-satoken/src/main/java/com/snail/common/satoken/config/SaTokenConfiguration.java
@@ -0,0 +1,42 @@
+package com.snail.common.satoken.config;
+
+import cn.dev33.satoken.dao.SaTokenDao;
+import cn.dev33.satoken.jwt.StpLogicJwtForSimple;
+import cn.dev33.satoken.stp.StpInterface;
+import cn.dev33.satoken.stp.StpLogic;
+import com.snail.common.satoken.core.dao.PlusSaTokenDao;
+import com.snail.common.satoken.core.service.SaPermissionImpl;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Sa-Token 配置
+ *
+ * @author Anfm
+ * Created time 2025/5/14
+ * @since 1.0
+ */
+@AutoConfiguration
+public class SaTokenConfiguration {
+
+    @Bean
+    public StpLogic getStpLogicJwt() {
+        return new StpLogicJwtForSimple();
+    }
+
+    /**
+     * 权限接口实现(使用bean注入方便用户替换)
+     */
+    @Bean
+    public StpInterface stpInterface() {
+        return new SaPermissionImpl();
+    }
+
+    /**
+     * 自定义dao层存储
+     */
+    @Bean
+    public SaTokenDao saTokenDao() {
+        return new PlusSaTokenDao();
+    }
+}
