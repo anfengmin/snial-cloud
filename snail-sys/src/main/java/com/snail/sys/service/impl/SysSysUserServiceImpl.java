0d77eb82eec0cea2d75eea00b06ddaae34276995
Levi <anfengmin@yeah.net>
2025-05-18 21:18:08 +0800

    feat: init

--- snail-sys/src/main/java/com/snail/sys/service/impl/SysSysUserServiceImpl.java
+++ snail-sys/src/main/java/com/snail/sys/service/impl/SysSysUserServiceImpl.java
@@ -3,10 +3,10 @@ package com.snail.sys.service.impl;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.ObjectUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.snail.sys.api.domain.User;
-import com.snail.sys.api.vo.UserVo;
-import com.snail.sys.dao.UserDao;
-import com.snail.sys.service.UserService;
+import com.snail.sys.api.domain.SysUser;
+import com.snail.sys.api.vo.SysUserVo;
+import com.snail.sys.dao.SysUserDao;
+import com.snail.sys.service.SysUserService;
 import com.snial.common.core.constant.UserConstants;
 import com.snial.common.core.exception.user.UserException;
 import lombok.extern.slf4j.Slf4j;
@@ -20,7 +20,7 @@ import org.springframework.stereotype.Service;
  */
 @Slf4j
 @Service("userService")
-public class UserServiceImpl extends ServiceImpl<UserDao, User> implements UserService {
+public class SysSysUserServiceImpl extends ServiceImpl<SysUserDao, SysUser> implements SysUserService {
 
     /**
      * 根据用户账号获取用户信息
@@ -29,17 +29,17 @@ public class UserServiceImpl extends ServiceImpl<UserDao, User> implements UserS
      * @return 用户信息
      */
     @Override
-    public UserVo getUserInfo(String userCode) {
-        User userInfo = this.lambdaQuery().eq(User::getUserCode, userCode).one();
-        if (ObjectUtil.isEmpty(userInfo)) {
+    public SysUserVo getUserInfo(String userCode) {
+        SysUser sysUserInfo = this.lambdaQuery().eq(com.snail.sys.api.domain.SysUser::getUserCode, userCode).one();
+        if (ObjectUtil.isEmpty(sysUserInfo)) {
             throw new UserException("用户不存在", userCode);
         }
-        if (UserConstants.USER_DISABLE.equals(userInfo.getStatus())) {
+        if (UserConstants.USER_DISABLE.equals(sysUserInfo.getStatus())) {
             throw new UserException("账号已禁用", userCode);
         }
-        UserVo userVo = BeanUtil.copyProperties(userInfo, UserVo.class);
+        SysUserVo sysUserVo = BeanUtil.copyProperties(sysUserInfo, SysUserVo.class);
 
-        log.info("userInfo:{}", userInfo);
-        return userVo;
+        log.info("userInfo:{}", sysUserInfo);
+        return sysUserVo;
     }
 }
