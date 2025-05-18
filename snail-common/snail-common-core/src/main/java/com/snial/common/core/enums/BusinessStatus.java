bdbaa240a9c8103107ff27a60c402cc9cff71541
Levi <anfengmin@yeah.net>
2025-05-19 00:00:32 +0800

    feat: init

---
+++ snail-common/snail-common-core/src/main/java/com/snial/common/core/enums/BusinessStatus.java
@@ -0,0 +1,21 @@
+package com.snial.common.core.enums;
+
+/**
+ * 操作状态
+ *
+ * @author Levi.
+ * Created time 2025/5/18
+ * @since 1.0
+ */
+public enum BusinessStatus {
+
+    /**
+     * 成功
+     */
+    SUCCESS,
+
+    /**
+     * 失败
+     */
+    FAIL,
+}
