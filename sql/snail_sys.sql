03126d831c5573878d415f1693d4a2369b67fda3
Levi <anfengmin@yeah.net>
2025-05-18 20:41:33 +0800

    feat(sql): 添加岗位信息表并优化部门和用户表
    - 在数据库中添加岗位信息表 sys_post
    - 优化用户和部门表中的状态和删除标志字段注释
    - 更新字段类型：将部分 char(1) 类型改为 tinyint(1)

--- sql/snail_sys.sql
+++ sql/snail_sys.sql
@@ -15,8 +15,8 @@ create table sys_user
     sex         char(1)      default '0' comment '用户性别（0男 1女 2未知）',
     avatar      varchar(100) default '' comment '头像地址',
     pass_word   varchar(100) default '' comment '密码',
-    status      char(1)      default '0' comment '帐号状态（0正常 1停用）',
-    deleted     char(1)      default '0' comment '删除标志（0代表存在 1代表删除）',
+    status      char(1)      default '0' comment '帐号状态（0:正常 1:停用）',
+    deleted     char(1)      default '0' comment '删除标志（0:存在 1:删除）',
     login_ip    varchar(128) default '' comment '最后登录IP',
     login_date  datetime comment '最后登录时间',
     create_by   varchar(64)  default '' comment '创建者',
@@ -47,8 +47,8 @@ create table sys_dept
     leader      varchar(20)  default null comment '负责人',
     phone       varchar(11)  default null comment '联系电话',
     email       varchar(50)  default null comment '邮箱',
-    status      char(1)      default '0' comment '部门状态（0正常 1停用）',
-    deleted     char(1)      default '0' comment '删除标志（0代表存在 1代表删除）',
+    status      tinyint(1)   default 0 comment '部门状态（0:正常 1:停用）',
+    deleted     tinyint(1)   default 0 comment '删除标志（0:存在 1:删除）',
     create_by   varchar(64)  default '' comment '创建者',
     create_time datetime comment '创建时间',
     update_by   varchar(64)  default '' comment '更新者',
@@ -69,3 +69,30 @@ insert into sys_dept values(107,  101, '0,100,101',  '运维部门',   5, 'admin
 insert into sys_dept values(108,  102, '0,100,102',  '市场部门',   1, 'admin', '15888888888', 'ry@qq.com', '0', '0', 'admin', sysdate(), '', null);
 insert into sys_dept values(109,  102, '0,100,102',  '财务部门',   2, 'admin', '15888888888', 'ry@qq.com', '0', '0', 'admin', sysdate(), '', null);
 
+-- ----------------------------
+-- 3、岗位信息表
+-- ----------------------------
+drop table if exists sys_post;
+create table sys_post
+(
+    id          bigint(20)  not null comment '岗位ID',
+    post_code   varchar(64) not null comment '岗位编码',
+    post_name   varchar(50) not null comment '岗位名称',
+    post_sort   int(4)      not null comment '显示顺序',
+    status      tinyint(1)  not null comment '状态（0正常 1停用）',
+    deleted     tinyint(1)   default 0 comment '删除标志（0:存在 1:删除）',
+    create_by   varchar(64)  default '' comment '创建者',
+    create_time datetime comment '创建时间',
+    update_by   varchar(64)  default '' comment '更新者',
+    update_time datetime comment '更新时间',
+    remark      varchar(500) default null comment '备注',
+    primary key (id)
+) engine = innodb comment = '岗位信息';
+
+-- ----------------------------
+-- 初始化-岗位信息表数据
+-- ----------------------------
+insert into sys_post values(1, 'ceo',  '董事长',    1, 0,  0, 'admin', sysdate(), '', null, '');
+insert into sys_post values(2, 'se',   '项目经理',  2, 0,  0, 'admin', sysdate(), '', null, '');
+insert into sys_post values(3, 'hr',   '人力资源',  3, 0,  0, 'admin', sysdate(), '', null, '');
+insert into sys_post values(4, 'user', '普通员工',  4, 0,  0, 'admin', sysdate(), '', null, '');
