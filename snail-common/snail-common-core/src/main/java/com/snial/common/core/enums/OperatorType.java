13c204336b8b4d9014ffcd6657f7f53a959f22ed
Levi <anfengmin@yeah.net>
2025-05-19 00:01:04 +0800

    feat: init

---
+++ snail-common/snail-common-core/src/main/java/com/snial/common/core/enums/OperatorType.java
@@ -0,0 +1,26 @@
+package com.snial.common.core.enums;
+
+/**
+ * 操作人类别
+ *
+ * @author Levi.
+ * Created time 2025/5/18
+ * @since 1.0
+ */
+public enum OperatorType {
+
+    /**
+     * 其它
+     */
+    OTHER,
+
+    /**
+     * 后台用户
+     */
+    MANAGE,
+
+    /**
+     * 手机端用户
+     */
+    MOBILE
+}
