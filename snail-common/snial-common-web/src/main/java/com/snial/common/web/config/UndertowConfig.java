d0ab51145c5b322b5cf2af8e772a12a106e2e0aa
Levi <anfengmin@yeah.net>
2025-05-19 00:01:14 +0800

    feat: init

---
+++ snail-common/snial-common-web/src/main/java/com/snial/common/web/config/UndertowConfig.java
@@ -0,0 +1,33 @@
+package com.snial.common.web.config;
+
+import io.undertow.server.DefaultByteBufferPool;
+import io.undertow.websockets.jsr.WebSocketDeploymentInfo;
+import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+
+/**
+ * Undertow 自定义配置
+ *
+ * @author Levi.
+ * Created time 2025/5/18
+ * @since 1.0
+ */
+public class UndertowConfig implements WebServerFactoryCustomizer<UndertowServletWebServerFactory> {
+
+    /**
+     * 设置 Undertow 的 websocket 缓冲池
+     *
+     * @param factory factory
+     * @since 1.0
+     */
+    @Override
+    public void customize(UndertowServletWebServerFactory factory) {
+        // 默认不直接分配内存 如果项目中使用了 websocket 建议直接分配
+        factory.addDeploymentInfoCustomizers(deploymentInfo -> {
+            WebSocketDeploymentInfo webSocketDeploymentInfo = new WebSocketDeploymentInfo();
+            webSocketDeploymentInfo.setBuffers(new DefaultByteBufferPool(false, 512));
+            deploymentInfo.addServletContextAttribute("io.undertow.websockets.jsr.WebSocketDeploymentInfo", webSocketDeploymentInfo);
+        });
+    }
+
+}
