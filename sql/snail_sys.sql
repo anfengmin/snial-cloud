be6a11910fff2d9152872201b3d13516f4a7d1ea
Levi <anfengmin@yeah.net>
2025-05-18 20:24:10 +0800

    feat(database): 添加部门管理相关表结构
    - 新增 sys_dept表，用于存储部门信息
    - 表包含部门 ID、父部门 ID、祖级列表、部门名称、显示顺序、负责人、联系电话、邮箱、状态、删除标志等字段

--- sql/snail_sys.sql
+++ sql/snail_sys.sql
@@ -11,7 +11,7 @@ create table sys_user
     phone_no    varchar(11)  default '' comment '手机号码',
     sex         char(1)      default '0' comment '用户性别（0男 1女 2未知）',
     avatar      varchar(100) default '' comment '头像地址',
-    pass_word    varchar(100) default '' comment '密码',
+    pass_word   varchar(100) default '' comment '密码',
     status      char(1)      default '0' comment '帐号状态（0正常 1停用）',
     deleted     char(1)      default '0' comment '删除标志（0代表存在 2代表删除）',
     login_ip    varchar(128) default '' comment '最后登录IP',
@@ -26,20 +26,21 @@ create table sys_user
 
 
 drop table if exists sys_dept;
-create table sys_dept (
-                          id           bigint(20)      not null                   comment '部门id',
-                          parent_id         bigint(20)      default 0                  comment '父部门id',
-                          ancestors         varchar(500)    default ''                 comment '祖级列表',
-                          dept_name         varchar(30)     default ''                 comment '部门名称',
-                          order_no         int(4)          default 0                  comment '显示顺序',
-                          leader            varchar(20)     default null               comment '负责人',
-                          phone             varchar(11)     default null               comment '联系电话',
-                          email             varchar(50)     default null               comment '邮箱',
-                          status            char(1)         default '0'                comment '部门状态（0正常 1停用）',
-                          deleted          char(1)         default '0'                comment '删除标志（0代表存在 2代表删除）',
-                          create_by         varchar(64)     default ''                 comment '创建者',
-                          create_time 	    datetime                                   comment '创建时间',
-                          update_by         varchar(64)     default ''                 comment '更新者',
-                          update_time       datetime                                   comment '更新时间',
-                          primary key (id)
-) engine=innodb comment = '部门';
+create table sys_dept
+(
+    id          bigint(20) not null comment '部门id',
+    parent_id   bigint(20)   default 0 comment '父部门id',
+    ancestors   varchar(500) default '' comment '祖级列表',
+    dept_name   varchar(30)  default '' comment '部门名称',
+    order_no    int(4)       default 0 comment '显示顺序',
+    leader      varchar(20)  default null comment '负责人',
+    phone       varchar(11)  default null comment '联系电话',
+    email       varchar(50)  default null comment '邮箱',
+    status      char(1)      default '0' comment '部门状态（0正常 1停用）',
+    deleted     char(1)      default '0' comment '删除标志（0代表存在 2代表删除）',
+    create_by   varchar(64)  default '' comment '创建者',
+    create_time datetime comment '创建时间',
+    update_by   varchar(64)  default '' comment '更新者',
+    update_time datetime comment '更新时间',
+    primary key (id)
+) engine = innodb comment = '部门';
