04fb04eeeeeea520840927e1d8e66d91f866d463
Levi <anfengmin@yeah.net>
2025-05-19 00:01:01 +0800

    feat: init

---
+++ snail-common/snial-common-log/src/main/java/com/snial/common/log/event/OperateLogEvent.java
@@ -0,0 +1,106 @@
+package com.snial.common.log.event;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 操作日志事件
+ *
+ * @author Levi.
+ * Created time 2025/5/18
+ * @since 1.0
+ */
+@Data
+public class OperateLogEvent implements Serializable {
+
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 日志主键
+     */
+    private Long id;
+
+    /**
+     * 操作模块
+     */
+    private String title;
+
+    /**
+     * 业务类型（0其它 1新增 2修改 3删除）
+     */
+    private Integer businessType;
+
+    /**
+     * 业务类型数组
+     */
+    private Integer[] businessTypes;
+
+    /**
+     * 请求方法
+     */
+    private String method;
+
+    /**
+     * 请求方式
+     */
+    private String requestMethod;
+
+    /**
+     * 操作类别（0:其它 1:后台用户 2:手机端用户）
+     */
+    private Integer operatorType;
+
+    /**
+     * 操作人员
+     */
+    private String operatorName;
+
+    /**
+     * 部门名称
+     */
+    private String deptName;
+
+    /**
+     * 请求url
+     */
+    private String operateUrl;
+
+    /**
+     * 操作地址
+     */
+    private String operateIp;
+
+    /**
+     * 操作地点
+     */
+    private String operateLocation;
+
+    /**
+     * 请求参数
+     */
+    private String operateParam;
+
+    /**
+     * 返回参数
+     */
+    private String jsonResult;
+
+    /**
+     * 操作状态（0正常 1异常）
+     */
+    private Integer status;
+
+    /**
+     * 错误消息
+     */
+    private String errorMsg;
+
+    /**
+     * 操作时间
+     */
+    private Date operateTime;
+}
