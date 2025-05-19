93fdf69bbed215fcef282c791cbf685589cfa26e
anfengmin <anfengmin@yeah.net>
2025-05-19 11:59:11 +0800

    feat:moved snial->snail

--- snail-common/snail-common-core/src/main/java/com/snail/common/core/utils/SpringUtils.java
+++ snail-common/snail-common-core/src/main/java/com/snail/common/core/utils/SpringUtils.java
@@ -1,9 +1,8 @@
-package com.snial.common.core.utils;
+package com.snail.common.core.utils;
 
 import cn.hutool.extra.spring.SpringUtil;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 /**
