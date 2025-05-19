c1a4fd38fde5c04b2ca099db5c59025ba223398b
anfengmin <anfengmin@yeah.net>
2025-05-19 11:59:23 +0800

    feat:moved snial->snail

--- snail-sys/src/main/java/com/snail/auth/controller/OauthController.java
+++ snail-sys/src/main/java/com/snail/auth/controller/OauthController.java
@@ -5,15 +5,14 @@ import cn.hutool.http.HttpResponse;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.snail.sys.api.form.LoginBody;
-import com.snial.common.core.utils.MessageUtils;
-import com.snial.common.core.utils.R;
+import com.snail.common.core.utils.MessageUtils;
+import com.snail.common.core.utils.R;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.constraints.NotBlank;
 import java.util.Map;
 
 /**
@@ -34,19 +33,31 @@ public class OauthController {
     @ApiModelProperty(value = "登录")
     @PostMapping("token")
     public R<Map<String, Object>> token(@ModelAttribute LoginBody form) {
+
         log.info("登录信息：{}", form);
-        HttpResponse response = HttpRequest.post("").form("", "").execute();
-        int status = response.getStatus();
-        if (status != 200) {
-            return R.fail("登录失败");
-        }
-        String body = response.body();
-        JSONObject entries = JSONUtil.parseObj(body);
-        log.info("登录信息：{}", entries);
-        // body转换成json
+        JSONObject user = getUser();
+
 
         return R.ok();
     }
+
+    private  JSONObject getUser() {
+        HttpResponse response;
+        JSONObject entries;
+        try {
+            response = HttpRequest.post("").form("", "").execute();
+            int status = response.getStatus();
+            if (status != 200) {
+                throw new RuntimeException("登录失败");
+            }
+            String body = response.body();
+            return JSONUtil.parseObj(body);
+        } catch (Exception e) {
+            log.error("登录失败信息：{}", e);
+            throw new RuntimeException(e);
+        }
+    }
+
     @GetMapping("/test1")
     public R<Void> test1(String code) {
         return R.ok(MessageUtils.message(code));
