5d0a079231ea61e508fcc61c9cdebf384df6d8b8
Levi <anfengmin@yeah.net>
2025-05-21 21:57:15 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/dao/SysDictTypeDao.java
@@ -0,0 +1,16 @@
+package com.snail.sys.dao;
+
+import com.snail.sys.domain.SysDictType;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+
+/**
+ * 字典类型(SysDictType)表数据库访问层
+ *
+ * @author makejava
+ * @since 2025-05-21 21:48:36
+ */
+public interface SysDictTypeDao extends BaseMapper<SysDictType> {
+
+}
+
