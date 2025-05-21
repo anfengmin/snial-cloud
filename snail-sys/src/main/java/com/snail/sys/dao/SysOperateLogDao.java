2a30c4f1c8c88cd3aeb62b9039a7d5c029375bd8
Levi <anfengmin@yeah.net>
2025-05-22 07:30:44 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/dao/SysOperateLogDao.java
@@ -0,0 +1,16 @@
+package com.snail.sys.dao;
+
+import com.snail.sys.domain.SysOperateLog;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+
+/**
+ * 操作日志记录(SysOperateLog)表数据库访问层
+ *
+ * @author makejava
+ * @since 2025-05-21 21:52:04
+ */
+public interface SysOperateLogDao extends BaseMapper<SysOperateLog> {
+
+}
+
