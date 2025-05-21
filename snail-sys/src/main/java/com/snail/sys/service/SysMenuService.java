0f8f03706d817be7291730cf7383862772661652
Levi <anfengmin@yeah.net>
2025-05-21 21:57:46 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/service/SysMenuService.java
@@ -0,0 +1,16 @@
+package com.snail.sys.service;
+
+import com.snail.sys.domain.SysMenu;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+
+/**
+ * 菜单权限表(SysMenu)表服务接口
+ *
+ * @author makejava
+ * @since 2025-05-21 21:36:54
+ */
+public interface SysMenuService extends IService<SysMenu> {
+
+
+}
