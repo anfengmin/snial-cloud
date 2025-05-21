319b6445f5bb6c16415de55094ce32265d4e7078
Levi <anfengmin@yeah.net>
2025-05-21 21:58:32 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/service/SysOssConfigService.java
@@ -0,0 +1,16 @@
+package com.snail.sys.service;
+
+import com.snail.sys.domain.SysOssConfig;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+
+/**
+ * 对象存储配置表(SysOssConfig)表服务接口
+ *
+ * @author makejava
+ * @since 2025-05-21 21:52:58
+ */
+public interface SysOssConfigService extends IService<SysOssConfig> {
+
+
+}
