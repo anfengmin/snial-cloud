0db0ed00afe56c23f32f3c610023f461f2865f8b
Levi <anfengmin@yeah.net>
2025-05-21 21:58:30 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/dao/SysOssConfigDao.java
@@ -0,0 +1,16 @@
+package com.snail.sys.dao;
+
+import com.snail.sys.domain.SysOssConfig;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+
+/**
+ * 对象存储配置表(SysOssConfig)表数据库访问层
+ *
+ * @author makejava
+ * @since 2025-05-21 21:52:59
+ */
+public interface SysOssConfigDao extends BaseMapper<SysOssConfig> {
+
+}
+
