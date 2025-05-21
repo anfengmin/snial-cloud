39dc4fb4b124fe5181195381dcad02afe3ed360f
Levi <anfengmin@yeah.net>
2025-05-21 21:57:44 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/dao/SysMenuDao.java
@@ -0,0 +1,16 @@
+package com.snail.sys.dao;
+
+import com.snail.sys.domain.SysMenu;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+
+/**
+ * 菜单权限表(SysMenu)表数据库访问层
+ *
+ * @author makejava
+ * @since 2025-05-21 21:36:54
+ */
+public interface SysMenuDao extends BaseMapper<SysMenu> {
+
+}
+
