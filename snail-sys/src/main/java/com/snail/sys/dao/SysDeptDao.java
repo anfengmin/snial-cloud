7b76b0df45c34b0535d17fa6e912024eba65874b
Levi <anfengmin@yeah.net>
2025-05-21 21:56:46 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/dao/SysDeptDao.java
@@ -0,0 +1,16 @@
+package com.snail.sys.dao;
+
+import com.snail.sys.domain.SysDept;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+
+/**
+ * 部门表(SysDept)表数据库访问层
+ *
+ * @author makejava
+ * @since 2025-05-21 21:30:47
+ */
+public interface SysDeptDao extends BaseMapper<SysDept> {
+
+}
+
