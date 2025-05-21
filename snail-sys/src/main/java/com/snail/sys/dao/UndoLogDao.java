db555141a2e252826ef88a94503372c115be0fc9
Levi <anfengmin@yeah.net>
2025-05-22 07:32:05 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/dao/UndoLogDao.java
@@ -0,0 +1,16 @@
+package com.snail.sys.dao;
+
+import com.snail.sys.domain.UndoLog;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+
+/**
+ * AT transaction mode undo table(UndoLog)表数据库访问层
+ *
+ * @author makejava
+ * @since 2025-05-21 21:53:04
+ */
+public interface UndoLogDao extends BaseMapper<UndoLog> {
+
+}
+
