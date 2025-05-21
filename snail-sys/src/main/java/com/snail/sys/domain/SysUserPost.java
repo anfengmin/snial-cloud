c3593403a6c2c40b39d1a8e04a16dfc45dba7619
Levi <anfengmin@yeah.net>
2025-05-22 07:32:16 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/domain/SysUserPost.java
@@ -0,0 +1,31 @@
+package com.snail.sys.domain;
+
+import com.baomidou.mybatisplus.annotation.*;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 用户与岗位关联表(SysUserPost)实体类
+ *
+ * @author makejava
+ * @since 2025-05-21 21:53:03
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@TableName("sys_user_post")
+@ApiModel(value = "用户与岗位关联表")
+public class SysUserPost extends Model<SysUserPost> {
+
+    private static final long serialVersionUID = -52753061857241092L;
+
+
+    @ApiModelProperty(value = "用户ID")
+    private Long userId;
+
+    @ApiModelProperty(value = "岗位ID")
+    private Long postId;
+
+}
