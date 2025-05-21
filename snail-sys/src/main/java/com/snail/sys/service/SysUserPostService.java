620157e3226f82e3d9da76c071e45e38457dc761
Levi <anfengmin@yeah.net>
2025-05-22 07:31:53 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/service/SysUserPostService.java
@@ -0,0 +1,16 @@
+package com.snail.sys.service;
+
+import com.snail.sys.domain.SysUserPost;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+
+/**
+ * 用户与岗位关联表(SysUserPost)表服务接口
+ *
+ * @author makejava
+ * @since 2025-05-21 21:53:03
+ */
+public interface SysUserPostService extends IService<SysUserPost> {
+
+
+}
