6231948c874d85d37bb02b55292b2ed0e603ad8e
Levi <anfengmin@yeah.net>
2025-05-21 21:57:59 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/dao/SysNoticeDao.java
@@ -0,0 +1,16 @@
+package com.snail.sys.dao;
+
+import com.snail.sys.domain.SysNotice;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+
+/**
+ * 通知公告(SysNotice)表数据库访问层
+ *
+ * @author makejava
+ * @since 2025-05-21 21:51:15
+ */
+public interface SysNoticeDao extends BaseMapper<SysNotice> {
+
+}
+
