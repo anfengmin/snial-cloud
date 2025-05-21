74eeec3fea2858370dad0bec088d0ae36ee210ef
Levi <anfengmin@yeah.net>
2025-05-22 07:31:04 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/dao/SysRoleDao.java
@@ -0,0 +1,16 @@
+package com.snail.sys.dao;
+
+import com.snail.sys.domain.SysRole;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+
+/**
+ * 角色信息(SysRole)表数据库访问层
+ *
+ * @author makejava
+ * @since 2025-05-21 21:53:01
+ */
+public interface SysRoleDao extends BaseMapper<SysRole> {
+
+}
+
