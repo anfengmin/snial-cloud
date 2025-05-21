37e67e544d2f1eae70f03c36949d665117a8e127
Levi <anfengmin@yeah.net>
2025-05-22 07:31:10 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/domain/SysRoleDept.java
@@ -0,0 +1,31 @@
+package com.snail.sys.domain;
+
+import com.baomidou.mybatisplus.annotation.*;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 角色和部门关联(SysRoleDept)实体类
+ *
+ * @author makejava
+ * @since 2025-05-21 21:53:01
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@TableName("sys_role_dept")
+@ApiModel(value = "角色和部门关联")
+public class SysRoleDept extends Model<SysRoleDept> {
+
+    private static final long serialVersionUID = -73263080247909543L;
+
+
+    @ApiModelProperty(value = "角色ID")
+    private Long roleId;
+
+    @ApiModelProperty(value = "部门ID")
+    private Long deptId;
+
+}
