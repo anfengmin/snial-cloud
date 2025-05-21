d3a0e15c42f8c8900dba29d8513a4b696279be57
Levi <anfengmin@yeah.net>
2025-05-22 07:31:37 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/dao/SysRoleMenuDao.java
@@ -0,0 +1,16 @@
+package com.snail.sys.dao;
+
+import com.snail.sys.domain.SysRoleMenu;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+
+/**
+ * 角色和菜单关联(SysRoleMenu)表数据库访问层
+ *
+ * @author makejava
+ * @since 2025-05-21 21:53:02
+ */
+public interface SysRoleMenuDao extends BaseMapper<SysRoleMenu> {
+
+}
+
