33954337cf37cc0d2749ef4f35426bd327ae8fbc
Levi <anfengmin@yeah.net>
2025-05-22 07:31:27 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/service/SysRoleMenuService.java
@@ -0,0 +1,16 @@
+package com.snail.sys.service;
+
+import com.snail.sys.domain.SysRoleMenu;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+
+/**
+ * 角色和菜单关联(SysRoleMenu)表服务接口
+ *
+ * @author makejava
+ * @since 2025-05-21 21:53:02
+ */
+public interface SysRoleMenuService extends IService<SysRoleMenu> {
+
+
+}
