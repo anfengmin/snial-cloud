1b8d12929013bab63d051c62481fd892e66a68b9
Levi <anfengmin@yeah.net>
2025-05-21 21:57:32 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/service/SysLoginInfoService.java
@@ -0,0 +1,16 @@
+package com.snail.sys.service;
+
+import com.snail.sys.domain.SysLoginInfo;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+
+/**
+ * 系统访问记录(SysLoginInfo)表服务接口
+ *
+ * @author makejava
+ * @since 2025-05-21 21:50:06
+ */
+public interface SysLoginInfoService extends IService<SysLoginInfo> {
+
+
+}
