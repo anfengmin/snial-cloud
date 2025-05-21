a128a6242331ba9708ec5f89cf4cd68ebb52ed08
Levi <anfengmin@yeah.net>
2025-05-22 07:32:27 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/service/SysOperateLogService.java
@@ -0,0 +1,16 @@
+package com.snail.sys.service;
+
+import com.snail.sys.domain.SysOperateLog;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+
+/**
+ * 操作日志记录(SysOperateLog)表服务接口
+ *
+ * @author makejava
+ * @since 2025-05-21 21:52:04
+ */
+public interface SysOperateLogService extends IService<SysOperateLog> {
+
+
+}
