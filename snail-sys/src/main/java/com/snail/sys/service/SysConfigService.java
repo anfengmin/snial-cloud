fc8826cade5685aa40303c315537f5f40481def5
Levi <anfengmin@yeah.net>
2025-05-21 21:56:33 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/service/SysConfigService.java
@@ -0,0 +1,16 @@
+package com.snail.sys.service;
+
+import com.snail.sys.domain.SysConfig;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+
+/**
+ * 参数配置表(SysConfig)表服务接口
+ *
+ * @author makejava
+ * @since 2025-05-21 21:32:37
+ */
+public interface SysConfigService extends IService<SysConfig> {
+
+
+}
