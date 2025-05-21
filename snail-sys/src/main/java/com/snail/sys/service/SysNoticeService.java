b5ac3b3728f2f9aa040d118ba9a3793d268d68a9
Levi <anfengmin@yeah.net>
2025-05-21 21:58:03 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/service/SysNoticeService.java
@@ -0,0 +1,16 @@
+package com.snail.sys.service;
+
+import com.snail.sys.domain.SysNotice;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+
+/**
+ * 通知公告(SysNotice)表服务接口
+ *
+ * @author makejava
+ * @since 2025-05-21 21:51:15
+ */
+public interface SysNoticeService extends IService<SysNotice> {
+
+
+}
