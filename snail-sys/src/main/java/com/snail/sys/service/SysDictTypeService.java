625fdc7a934f12d5dab753eb9cfc944591c854cc
Levi <anfengmin@yeah.net>
2025-05-21 21:57:18 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/service/SysDictTypeService.java
@@ -0,0 +1,16 @@
+package com.snail.sys.service;
+
+import com.snail.sys.domain.SysDictType;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+
+/**
+ * 字典类型(SysDictType)表服务接口
+ *
+ * @author makejava
+ * @since 2025-05-21 21:48:36
+ */
+public interface SysDictTypeService extends IService<SysDictType> {
+
+
+}
