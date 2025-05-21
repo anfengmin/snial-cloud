6ac2830f2d40d7ae7fe09c2a1c1528ac155e0623
Levi <anfengmin@yeah.net>
2025-05-21 21:58:15 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/service/SysOssService.java
@@ -0,0 +1,16 @@
+package com.snail.sys.service;
+
+import com.snail.sys.domain.SysOss;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+
+/**
+ * OSS对象存储(SysOss)表服务接口
+ *
+ * @author makejava
+ * @since 2025-05-21 21:52:20
+ */
+public interface SysOssService extends IService<SysOss> {
+
+
+}
