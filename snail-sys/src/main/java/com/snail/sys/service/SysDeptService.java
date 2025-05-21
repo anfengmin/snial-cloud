d6a83b9eef4d29cf48ab1aefaa5de5d300d0dcb2
Levi <anfengmin@yeah.net>
2025-05-21 21:56:49 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/service/SysDeptService.java
@@ -0,0 +1,16 @@
+package com.snail.sys.service;
+
+import com.snail.sys.domain.SysDept;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+
+/**
+ * 部门表(SysDept)表服务接口
+ *
+ * @author makejava
+ * @since 2025-05-21 21:30:46
+ */
+public interface SysDeptService extends IService<SysDept> {
+
+
+}
