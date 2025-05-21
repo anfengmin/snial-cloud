ded9ccfad0f7e73207d616245b354d21022177f6
Levi <anfengmin@yeah.net>
2025-05-22 07:32:30 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/service/UndoLogService.java
@@ -0,0 +1,16 @@
+package com.snail.sys.service;
+
+import com.snail.sys.domain.UndoLog;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+
+/**
+ * AT transaction mode undo table(UndoLog)表服务接口
+ *
+ * @author makejava
+ * @since 2025-05-21 21:53:04
+ */
+public interface UndoLogService extends IService<UndoLog> {
+
+
+}
