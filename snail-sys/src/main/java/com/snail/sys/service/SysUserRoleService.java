ef70719a1eee2bf4179517ab7b981fda3816a6eb
Levi <anfengmin@yeah.net>
2025-05-22 07:32:23 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/service/SysUserRoleService.java
@@ -0,0 +1,16 @@
+package com.snail.sys.service;
+
+import com.snail.sys.domain.SysUserRole;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+
+/**
+ * 用户和角色关联表(SysUserRole)表服务接口
+ *
+ * @author makejava
+ * @since 2025-05-21 21:53:04
+ */
+public interface SysUserRoleService extends IService<SysUserRole> {
+
+
+}
