3b4dd3bcf6491ff4fa2104b96b708d893edb0c7b
Levi <anfengmin@yeah.net>
2025-05-21 21:57:41 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/controller/SysMenuController.java
@@ -0,0 +1,54 @@
+package com.snail.sys.controller;
+
+import com.snail.common.core.utils.R;
+import com.snail.sys.domain.SysMenu;
+import com.snail.sys.service.SysMenuService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * 菜单权限表(SysMenu)表控制层
+ *
+ * @author makejava
+ * @since 2025-05-21 21:36:54
+ */
+@Api(tags = "菜单权限表")
+@RestController
+@RequestMapping("/v1/sysMenu")
+public class SysMenuController {
+
+    @Resource
+    private SysMenuService sysMenuService;
+
+
+
+    @GetMapping("{id}")
+    @ApiOperation(value = "主键查询")
+    public R<SysMenu> queryById(@PathVariable("id") Long id) {
+        return R.ok(sysMenuService.getById(id));
+    }
+
+    @PostMapping
+    @ApiOperation(value = "新增数据")
+    public R<Boolean> add(SysMenu sysMenu) {
+        return R.ok(sysMenuService.save(sysMenu));
+    }
+
+    @PutMapping
+    @ApiOperation(value = "编辑数据")
+    public R<Boolean> edit(SysMenu sysMenu) {
+        return R.ok(sysMenuService.updateById(sysMenu));
+    }
+
+    @DeleteMapping
+    @ApiOperation(value = "删除数据")
+    public R<Boolean> deleteById(@RequestParam("ids") List<Long> ids) {
+        return R.ok(sysMenuService.removeByIds(ids));
+    }
+
+}
+
