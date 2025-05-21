be5a4b025cd731108a21fb845cca7046e1f5949e
Levi <anfengmin@yeah.net>
2025-05-22 07:31:43 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/dao/SysUserPostDao.java
@@ -0,0 +1,16 @@
+package com.snail.sys.dao;
+
+import com.snail.sys.domain.SysUserPost;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+
+/**
+ * 用户与岗位关联表(SysUserPost)表数据库访问层
+ *
+ * @author makejava
+ * @since 2025-05-21 21:53:03
+ */
+public interface SysUserPostDao extends BaseMapper<SysUserPost> {
+
+}
+
