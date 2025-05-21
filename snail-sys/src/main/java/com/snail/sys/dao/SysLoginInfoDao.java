845dd87c33f380c83539cb951868a422366b0b54
Levi <anfengmin@yeah.net>
2025-05-21 21:57:30 +0800

    feat: init

---
+++ snail-sys/src/main/java/com/snail/sys/dao/SysLoginInfoDao.java
@@ -0,0 +1,16 @@
+package com.snail.sys.dao;
+
+import com.snail.sys.domain.SysLoginInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+
+/**
+ * 系统访问记录(SysLoginInfo)表数据库访问层
+ *
+ * @author makejava
+ * @since 2025-05-21 21:50:07
+ */
+public interface SysLoginInfoDao extends BaseMapper<SysLoginInfo> {
+
+}
+
