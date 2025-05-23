9eb48fa2c783aefcb441356dcd7683931c0d8595
Levi <anfengmin@yeah.net>
2025-05-23 23:40:27 +0800

    feat: init

--- snail-sys/src/main/java/com/snail/sys/service/impl/SysDeptServiceImpl.java
+++ snail-sys/src/main/java/com/snail/sys/service/impl/SysDeptServiceImpl.java
@@ -1,5 +1,6 @@
 package com.snail.sys.service.impl;
 
+import cn.hutool.core.util.ObjectUtil;
 import com.snail.sys.domain.SysDept;
 import com.snail.sys.dao.SysDeptDao;
 import com.snail.sys.service.SysDeptService;
@@ -8,6 +9,7 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * 部门表(SysDept)表服务实现类
@@ -22,4 +24,23 @@ public class SysDeptServiceImpl extends ServiceImpl<SysDeptDao, SysDept> impleme
     private SysDeptDao sysDeptDao;
 
 
+    /**
+     * 获取部门列表
+     *
+     * @param dept dept
+     * @return java.util.List<com.snail.sys.domain.SysDept>
+     * @since 1.0
+     */
+    @Override
+    public List<SysDept> queryDeptList(SysDept dept) {
+
+        return this.lambdaQuery()
+                .eq(ObjectUtil.isNotNull(dept.getId()), SysDept::getId, dept.getId())
+                .eq(ObjectUtil.isNotNull(dept.getParentId()), SysDept::getParentId, dept.getParentId())
+                .like(ObjectUtil.isNotNull(dept.getDeptName()), SysDept::getDeptName, dept.getDeptName())
+                .eq(ObjectUtil.isNotNull(dept.getStatus()), SysDept::getStatus, dept.getStatus())
+                .orderByAsc(SysDept::getParentId)
+                .orderByAsc(SysDept::getOrderNo)
+                .list();
+    }
 }
