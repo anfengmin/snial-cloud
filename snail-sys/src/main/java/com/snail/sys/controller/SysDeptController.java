5fb20b4e59e2e7a6db25ee5c749554a9f4ce1dfe
Levi <anfengmin@yeah.net>
2025-05-24 00:09:44 +0800

    feat: init

--- snail-sys/src/main/java/com/snail/sys/controller/SysDeptController.java
+++ snail-sys/src/main/java/com/snail/sys/controller/SysDeptController.java
@@ -1,10 +1,14 @@
 package com.snail.sys.controller;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.convert.Convert;
+import cn.hutool.core.util.StrUtil;
 import com.snail.common.core.utils.R;
 import com.snail.sys.domain.SysDept;
 import com.snail.sys.service.SysDeptService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -18,7 +22,7 @@ import java.util.List;
  */
 @Api(tags = "部门表")
 @RestController
-@RequestMapping("/v1/sysDept")
+@RequestMapping("/v1/dept")
 public class SysDeptController {
 
     @Resource
@@ -32,6 +36,32 @@ public class SysDeptController {
         return R.ok(depts);
     }
 
+    @GetMapping("/list/exclude/{deptId}")
+    @ApiOperation(value = "查询部门列表（排除节点）")
+    public R<List<SysDept>> excludeChild(@PathVariable(value = "deptId", required = false) Long deptId) {
+        List<SysDept> depts = sysDeptService.queryDeptList(new SysDept());
+
+        depts.removeIf(d -> d.getId().intValue() == deptId
+                || CollUtil.contains(Convert.toList(String.class, StrUtil.split(d.getAncestors(), StrUtil.COMMA)), Convert.toStr(deptId)));
+
+        return R.ok(depts);
+    }
+
+
+    @GetMapping("/{deptId}")
+    @ApiOperation(value = "根据部门编号获取详细信息")
+    public R<SysDept> getInfo(@PathVariable Long deptId) {
+        return R.ok(sysDeptService.getById(deptId));
+    }
+
+    @PostMapping("/add")
+    @ApiOperation(value = "新增部门")
+    public R<Boolean> add(@Validated @RequestBody SysDept dept) {
+        if (sysDeptService.checkDeptNameUnique(dept)) {
+            return R.fail("新增部门'" + dept.getDeptName() + "'失败，部门名称已存在");
+        }
+        return R.isOk(sysDeptService.save(dept));
+    }
 
 }
 
