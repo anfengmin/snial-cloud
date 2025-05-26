d833b6d21d2a282818b8c6492208dc13892ce8ca
anfengmin <anfengmin@yeah.net>
2025-05-26 19:34:06 +0800

    feat:init

--- snail-sys/src/main/java/com/snail/sys/controller/SysDictDataController.java
+++ snail-sys/src/main/java/com/snail/sys/controller/SysDictDataController.java
@@ -1,13 +1,12 @@
 package com.snail.sys.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.snail.common.core.utils.R;
 import com.snail.sys.domain.SysDictData;
 import com.snail.sys.service.SysDictDataService;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
-import com.snail.common.core.utils.R;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -18,9 +17,9 @@ import java.util.List;
  * @author makejava
  * @since 2025-05-21 21:34:18
  */
-@Api(tags = "字典数据表")
+@Api(tags = "字典数据")
 @RestController
-@RequestMapping("/v1/sysDictData")
+@RequestMapping("/v1/dict/data")
 public class SysDictDataController {
 
     @Resource
@@ -28,10 +27,11 @@ public class SysDictDataController {
 
 
 
-    @GetMapping("{id}")
-    @ApiOperation(value = "主键查询")
-    public R<SysDictData> queryById(@PathVariable("id") Long id) {
-        return R.ok(sysDictDataService.getById(id));
+    @ApiOperation(value = "获取字典数据列表")
+    @GetMapping("/list")
+    public R<Page<SysDictData>> list(SysDictData dictData, Page<SysDictData> pageQuery) {
+        Page<SysDictData> page = sysDictDataService.lambdaQuery().page(pageQuery);
+        return R.ok(page);
     }
 
     @PostMapping
