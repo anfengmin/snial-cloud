da6c5dd800a2129316e916f2111c25c78014b521
Levi <anfengmin@yeah.net>
2025-05-27 22:27:40 +0800

    feat: init

--- snail-sys/src/main/java/com/snail/sys/service/SysDictDataService.java
+++ snail-sys/src/main/java/com/snail/sys/service/SysDictDataService.java
@@ -3,6 +3,8 @@ package com.snail.sys.service;
 import com.snail.sys.domain.SysDictData;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 
 /**
  * 字典数据表(SysDictData)表服务接口
@@ -13,4 +15,12 @@ import com.baomidou.mybatisplus.extension.service.IService;
 public interface SysDictDataService extends IService<SysDictData> {
 
 
+    /**
+     * queryDictDataByType
+     *
+     * @param dictType dictType
+     * @return java.util.List<com.snail.sys.domain.SysDictData>
+     * @since 1.0
+     */
+    List<SysDictData> queryDictDataByType(String dictType);
 }
