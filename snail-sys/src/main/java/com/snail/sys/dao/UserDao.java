278ea52cf0256baa468f7bca44912c165a9e3c9d
Levi <anfengmin@yeah.net>
2025-05-28 23:11:17 +0800

    feat: 系统用户

---
+++ snail-sys/src/main/java/com/snail/sys/dao/UserDao.java
@@ -0,0 +1,16 @@
+package com.snail.sys.dao;
+
+import com.snail.sys.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+
+/**
+ * 用户(User)表数据库访问层
+ *
+ * @author makejava
+ * @since 2025-05-28 22:44:49
+ */
+public interface UserDao extends BaseMapper<User> {
+
+}
+
