fadf6bee72124fe1eab869cca5ba3422e0e31b6a
Levi <anfengmin@yeah.net>
2025-05-28 22:26:07 +0800

    feat: 日志信息

--- snail-sys/src/main/java/com/snail/sys/service/SysLoginInfoService.java
+++ snail-sys/src/main/java/com/snail/sys/service/SysLoginInfoService.java
@@ -13,4 +13,8 @@ import com.baomidou.mybatisplus.extension.service.IService;
 public interface SysLoginInfoService extends IService<SysLoginInfo> {
 
 
+    /**
+     * 清空登录日志
+     */
+    void cleanLogInfo();
 }
