ee177ecbc8bb49dc7907226bb9a5893e0240f42e
Levi <anfengmin@yeah.net>
2025-05-29 21:55:13 +0800

    feat: 系统用户

--- snail-sys/src/main/java/com/snail/sys/service/impl/SysOperateLogServiceImpl.java
+++ snail-sys/src/main/java/com/snail/sys/service/impl/SysOperateLogServiceImpl.java
@@ -2,24 +2,34 @@ package com.snail.sys.service.impl;
 
 import com.snail.sys.domain.SysOperateLog;
 import com.snail.sys.dao.SysOperateLogDao;
+import com.snail.sys.dto.SysOperateLogPageDTO;
 import com.snail.sys.service.SysOperateLogService;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.snail.common.core.utils.R;
 import org.springframework.stereotype.Service;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 
 
-import javax.annotation.Resource;
-
 /**
- * 操作日志记录(SysOperateLog)表服务实现类
+ * 操作日志记录
  *
  * @author makejava
- * @since 2025-05-21 21:52:04
+ * @since 2025-05-29 21:52:18
  */
 @Service("sysOperateLogService")
 public class SysOperateLogServiceImpl extends ServiceImpl<SysOperateLogDao, SysOperateLog> implements SysOperateLogService {
 
-    @Resource
-    private SysOperateLogDao sysOperateLogDao;
 
-
+        /**
+         * 分页查询
+         *
+         * @param dto      筛选条件
+         * @return 查询结果
+         */
+        @Override
+        public R<Page<SysOperateLog>> queryByPage(SysOperateLogPageDTO dto) {
+            Page<SysOperateLog> page = new Page<>(dto.getCurrent(), dto.getSize());
+            Page<SysOperateLog> result = this.lambdaQuery().page(page);
+            return R.ok(result);
+        }
 }
