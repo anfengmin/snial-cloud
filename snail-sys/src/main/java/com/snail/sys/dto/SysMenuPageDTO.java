311c0bd6632c1236491bb57ea9470b4c2aa1bb4c
Levi <anfengmin@yeah.net>
2025-05-29 21:48:50 +0800

    feat: 系统用户

---
+++ snail-sys/src/main/java/com/snail/sys/dto/SysMenuPageDTO.java
@@ -0,0 +1,87 @@
+package com.snail.sys.dto;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import com.baomidou.mybatisplus.extension.plugins.pagination.PageDTO;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+
+/**
+ * 菜单权限(SysMenu)
+ *
+ * @author makejava
+ * @since 2025-05-29 21:45:02
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ApiModel(value = "菜单权限")
+public class SysMenuPageDTO extends PageDTO<SysMenu> implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "菜单ID")
+    private Long id;
+
+    @ApiModelProperty(value = "菜单名称")
+    private String menuName;
+
+    @ApiModelProperty(value = "父菜单ID")
+    private Long parentId;
+
+    @ApiModelProperty(value = "显示顺序")
+    private Integer orderNum;
+
+    @ApiModelProperty(value = "路由地址")
+    private String path;
+
+    @ApiModelProperty(value = "组件路径")
+    private String component;
+
+    @ApiModelProperty(value = "路由参数")
+    private String queryParam;
+
+    @ApiModelProperty(value = "是否为外链（0是 1否）")
+    private Integer isFrame;
+
+    @ApiModelProperty(value = "是否缓存（0缓存 1不缓存）")
+    private Integer isCache;
+
+    @ApiModelProperty(value = "菜单类型（M目录 C菜单 F按钮）")
+    private String menuType;
+
+    @ApiModelProperty(value = "显示状态（0显示 1隐藏）")
+    private Integer visible;
+
+    @ApiModelProperty(value = "状态（0正常 1停用）")
+    private Integer status;
+
+    @ApiModelProperty(value = "删除标志（0:存在 1:删除）")
+    private Integer deleted;
+
+    @ApiModelProperty(value = "权限标识")
+    private String perms;
+
+    @ApiModelProperty(value = "菜单图标")
+    private String icon;
+
+    @ApiModelProperty(value = "创建者")
+    private String createBy;
+
+    @ApiModelProperty(value = "创建时间")
+    private Date createTime;
+
+    @ApiModelProperty(value = "更新者")
+    private String updateBy;
+
+    @ApiModelProperty(value = "更新时间")
+    private Date updateTime;
+
+    @ApiModelProperty(value = "备注")
+    private String remark;
+
+}
