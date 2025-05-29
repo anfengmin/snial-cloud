fadc7451c8752bc57c946e725e1bd9748b5bc15b
Levi <anfengmin@yeah.net>
2025-05-29 21:55:10 +0800

    feat: 系统用户

--- snail-sys/src/main/java/com/snail/sys/service/SysOperateLogService.java
+++ snail-sys/src/main/java/com/snail/sys/service/SysOperateLogService.java
@@ -1,16 +1,27 @@
 package com.snail.sys.service;
 
 import com.snail.sys.domain.SysOperateLog;
+import com.snail.sys.dto.SysOperateLogPageDTO;
 import com.baomidou.mybatisplus.extension.service.IService;
-
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.snail.common.core.utils.R;
 
 /**
- * 操作日志记录(SysOperateLog)表服务接口
+ * 操作日志记录
  *
  * @author makejava
- * @since 2025-05-21 21:52:04
+ * @since 2025-05-29 21:52:17
  */
 public interface SysOperateLogService extends IService<SysOperateLog> {
+
+ /**
+     * 分页查询
+     *
+     * @param dto dto
+     * @return 分页结果
+     * @since 1.0
+     */
+     R<Page<SysOperateLog>> queryByPage(SysOperateLogPageDTO dto);
 
 
 }
