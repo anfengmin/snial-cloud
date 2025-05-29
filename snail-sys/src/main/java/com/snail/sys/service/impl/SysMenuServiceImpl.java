e8d7e101bd4a95853c626d5c2cf184dd98005018
Levi <anfengmin@yeah.net>
2025-05-29 21:48:56 +0800

    feat: 系统用户

--- snail-sys/src/main/java/com/snail/sys/service/impl/SysMenuServiceImpl.java
+++ snail-sys/src/main/java/com/snail/sys/service/impl/SysMenuServiceImpl.java
@@ -1,25 +1,36 @@
 package com.snail.sys.service.impl;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.snail.sys.domain.SysMenu;
 import com.snail.sys.dao.SysMenuDao;
+import com.snail.sys.dto.SysMenuPageDTO;
 import com.snail.sys.service.SysMenuService;
+import com.snail.common.core.utils.R;
 import org.springframework.stereotype.Service;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 
 
-import javax.annotation.Resource;
 
 /**
- * 菜单权限表(SysMenu)表服务实现类
+ * 菜单权限
  *
  * @author makejava
- * @since 2025-05-21 21:36:54
+ * @since 2025-05-29 21:45:02
  */
 @Service("sysMenuService")
 public class SysMenuServiceImpl extends ServiceImpl<SysMenuDao, SysMenu> implements SysMenuService {
 
-    @Resource
-    private SysMenuDao sysMenuDao;
 
-
+    /**
+     * 分页查询
+     *
+     * @param dto 筛选条件
+     * @return 查询结果
+     */
+    @Override
+    public R<Page<SysMenu>> queryByPage(SysMenuPageDTO dto) {
+        Page<SysMenu> page = new Page<>(dto.getCurrent(), dto.getSize());
+        Page<SysMenu> result = this.lambdaQuery().page(page);
+        return R.ok(result);
+    }
 }
