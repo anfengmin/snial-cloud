05cbe6f191dda83ad24b1fc9e103ede04a014a88
Levi <anfengmin@yeah.net>
2025-05-29 21:51:57 +0800

    feat: 系统用户

--- snail-sys/src/main/java/com/snail/sys/service/SysNoticeService.java
+++ snail-sys/src/main/java/com/snail/sys/service/SysNoticeService.java
@@ -1,16 +1,27 @@
 package com.snail.sys.service;
 
 import com.snail.sys.domain.SysNotice;
+import com.snail.sys.dto.SysNoticePageDTO;
 import com.baomidou.mybatisplus.extension.service.IService;
-
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.snail.common.core.utils.R;
 
 /**
- * 通知公告(SysNotice)表服务接口
+ * 通知公告
  *
  * @author makejava
- * @since 2025-05-21 21:51:15
+ * @since 2025-05-29 21:50:40
  */
 public interface SysNoticeService extends IService<SysNotice> {
+
+ /**
+     * 分页查询
+     *
+     * @param dto dto
+     * @return 分页结果
+     * @since 1.0
+     */
+     R<Page<SysNotice>> queryByPage(SysNoticePageDTO dto);
 
 
 }
