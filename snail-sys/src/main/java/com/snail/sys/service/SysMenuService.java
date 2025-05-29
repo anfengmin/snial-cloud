dd2b19c423bba58013c4f2f7901636b057d57650
Levi <anfengmin@yeah.net>
2025-05-29 21:48:53 +0800

    feat: 系统用户

--- snail-sys/src/main/java/com/snail/sys/service/SysMenuService.java
+++ snail-sys/src/main/java/com/snail/sys/service/SysMenuService.java
@@ -1,16 +1,27 @@
 package com.snail.sys.service;
 
-import com.snail.sys.domain.SysMenu;
 import com.baomidou.mybatisplus.extension.service.IService;
-
+import com.snail.common.core.utils.R;
+import com.snail.sys.domain.SysMenu;
+import com.snail.sys.dto.SysMenuPageDTO;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 
 /**
- * 菜单权限表(SysMenu)表服务接口
+ * 菜单权限
  *
  * @author makejava
- * @since 2025-05-21 21:36:54
+ * @since 2025-05-29 21:45:00
  */
 public interface SysMenuService extends IService<SysMenu> {
+
+    /**
+     * 分页查询
+     *
+     * @param dto dto
+     * @return 分页结果
+     * @since 1.0
+     */
+    R<Page<SysMenu>> queryByPage(SysMenuPageDTO dto);
 
 
 }
