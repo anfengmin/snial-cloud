aec2c16236559c9b2ebefda2abe9956dc9a5faaf
Levi <anfengmin@yeah.net>
2025-05-29 21:51:54 +0800

    feat: 系统用户

--- snail-sys/src/main/java/com/snail/sys/controller/SysNoticeController.java
+++ snail-sys/src/main/java/com/snail/sys/controller/SysNoticeController.java
@@ -1,29 +1,34 @@
 package com.snail.sys.controller;
-
-import com.snail.common.core.utils.R;
-import com.snail.sys.domain.SysNotice;
-import com.snail.sys.service.SysNoticeService;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import com.snail.sys.domain.SysNotice;
+import com.snail.sys.service.SysNoticeService;
+import com.snail.sys.dto.SysNoticePageDTO;
 import org.springframework.web.bind.annotation.*;
-
+import com.snail.common.core.utils.R;
 import javax.annotation.Resource;
 import java.util.List;
 
 /**
- * 通知公告(SysNotice)表控制层
+ * 通知公告
  *
  * @author makejava
- * @since 2025-05-21 21:51:14
+ * @since 2025-05-29 21:50:39
  */
 @Api(tags = "通知公告")
 @RestController
 @RequestMapping("/v1/sysNotice")
 public class SysNoticeController {
-
+    
     @Resource
     private SysNoticeService sysNoticeService;
-
+    
+    @PostMapping("queryByPage")
+    @ApiOperation(value = "分页查询", notes = "分页查询")
+    public R<Page<SysNotice>>queryByPage(@RequestBody SysNoticePageDTO dto){
+        return sysNoticeService.queryByPage(dto);
+    }
 
     @GetMapping("{id}")
     @ApiOperation(value = "主键查询")
