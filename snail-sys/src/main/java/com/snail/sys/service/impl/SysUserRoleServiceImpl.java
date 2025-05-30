e02a699f28fe84376feec4bacb8c9a336e3ab39c
Levi <anfengmin@yeah.net>
2025-05-30 23:18:23 +0800

    feat: EASTCODE

--- snail-sys/src/main/java/com/snail/sys/service/impl/SysUserRoleServiceImpl.java
+++ snail-sys/src/main/java/com/snail/sys/service/impl/SysUserRoleServiceImpl.java
@@ -2,24 +2,34 @@ package com.snail.sys.service.impl;
 
 import com.snail.sys.domain.SysUserRole;
 import com.snail.sys.dao.SysUserRoleDao;
+import com.snail.sys.dto.SysUserRolePageDTO;
 import com.snail.sys.service.SysUserRoleService;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.snail.common.core.utils.R;
 import org.springframework.stereotype.Service;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 
 
-import javax.annotation.Resource;
-
 /**
- * 用户和角色关联表(SysUserRole)表服务实现类
+ * 用户和角色关联表
  *
  * @author makejava
- * @since 2025-05-21 21:53:04
+ * @since 2025-05-30 23:14:08
  */
 @Service("sysUserRoleService")
 public class SysUserRoleServiceImpl extends ServiceImpl<SysUserRoleDao, SysUserRole> implements SysUserRoleService {
 
-    @Resource
-    private SysUserRoleDao sysUserRoleDao;
 
-
+    /**
+     * 分页查询
+     *
+     * @param dto 筛选条件
+     * @return 查询结果
+     */
+    @Override
+    public R<Page<SysUserRole>> queryByPage(SysUserRolePageDTO dto) {
+        Page<SysUserRole> page = new Page<>(dto.getCurrent(), dto.getSize());
+        Page<SysUserRole> result = this.lambdaQuery().page(page);
+        return R.ok(result);
+    }
 }
