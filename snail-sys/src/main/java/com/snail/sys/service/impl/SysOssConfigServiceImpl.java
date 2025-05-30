4e61c7e663f45c7ab17f697d6d56c00ba6b7f503
Levi <anfengmin@yeah.net>
2025-05-30 23:16:56 +0800

    feat: EASTCODE

--- snail-sys/src/main/java/com/snail/sys/service/impl/SysOssConfigServiceImpl.java
+++ snail-sys/src/main/java/com/snail/sys/service/impl/SysOssConfigServiceImpl.java
@@ -2,24 +2,34 @@ package com.snail.sys.service.impl;
 
 import com.snail.sys.domain.SysOssConfig;
 import com.snail.sys.dao.SysOssConfigDao;
+import com.snail.sys.dto.SysOssConfigPageDTO;
 import com.snail.sys.service.SysOssConfigService;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.snail.common.core.utils.R;
 import org.springframework.stereotype.Service;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 
 
-import javax.annotation.Resource;
-
 /**
- * 对象存储配置表(SysOssConfig)表服务实现类
+ * 对象存储配置表
  *
  * @author makejava
- * @since 2025-05-21 21:52:58
+ * @since 2025-05-30 23:04:59
  */
 @Service("sysOssConfigService")
 public class SysOssConfigServiceImpl extends ServiceImpl<SysOssConfigDao, SysOssConfig> implements SysOssConfigService {
 
-    @Resource
-    private SysOssConfigDao sysOssConfigDao;
 
-
+    /**
+     * 分页查询
+     *
+     * @param dto 筛选条件
+     * @return 查询结果
+     */
+    @Override
+    public R<Page<SysOssConfig>> queryByPage(SysOssConfigPageDTO dto) {
+        Page<SysOssConfig> page = new Page<>(dto.getCurrent(), dto.getSize());
+        Page<SysOssConfig> result = this.lambdaQuery().page(page);
+        return R.ok(result);
+    }
 }
