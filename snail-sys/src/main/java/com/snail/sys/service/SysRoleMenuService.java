71a29020e980f51cda15114351967a50c371f1c2
Levi <anfengmin@yeah.net>
2025-05-30 23:17:38 +0800

    feat: EASTCODE

--- snail-sys/src/main/java/com/snail/sys/service/SysRoleMenuService.java
+++ snail-sys/src/main/java/com/snail/sys/service/SysRoleMenuService.java
@@ -1,16 +1,27 @@
 package com.snail.sys.service;
 
 import com.snail.sys.domain.SysRoleMenu;
+import com.snail.sys.dto.SysRoleMenuPageDTO;
 import com.baomidou.mybatisplus.extension.service.IService;
-
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.snail.common.core.utils.R;
 
 /**
- * 角色和菜单关联(SysRoleMenu)表服务接口
+ * 角色和菜单关联
  *
  * @author makejava
- * @since 2025-05-21 21:53:02
+ * @since 2025-05-30 23:06:44
  */
 public interface SysRoleMenuService extends IService<SysRoleMenu> {
+
+    /**
+     * 分页查询
+     *
+     * @param dto dto
+     * @return 分页结果
+     * @since 1.0
+     */
+    R<Page<SysRoleMenu>> queryByPage(SysRoleMenuPageDTO dto);
 
 
 }
