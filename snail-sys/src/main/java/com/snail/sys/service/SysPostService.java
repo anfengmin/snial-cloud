69a59ddcf7e22e2b076d4d378c9f9fbb49d6b19b
Levi <anfengmin@yeah.net>
2025-05-30 23:14:52 +0800

    feat: EASTCODE

--- snail-sys/src/main/java/com/snail/sys/service/SysPostService.java
+++ snail-sys/src/main/java/com/snail/sys/service/SysPostService.java
@@ -1,16 +1,27 @@
 package com.snail.sys.service;
 
 import com.snail.sys.domain.SysPost;
+import com.snail.sys.dto.SysPostPageDTO;
 import com.baomidou.mybatisplus.extension.service.IService;
-
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.snail.common.core.utils.R;
 
 /**
- * 岗位信息(SysPost)表服务接口
+ * 岗位信息
  *
  * @author makejava
- * @since 2025-05-21 21:52:59
+ * @since 2025-05-30 23:05:46
  */
 public interface SysPostService extends IService<SysPost> {
+
+ /**
+     * 分页查询
+     *
+     * @param dto dto
+     * @return 分页结果
+     * @since 1.0
+     */
+     R<Page<SysPost>> queryByPage(SysPostPageDTO dto);
 
 
 }
