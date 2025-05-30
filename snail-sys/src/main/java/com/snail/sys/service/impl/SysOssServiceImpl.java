af6d8a0c3854591b525c6bfecfe49041d2fb00fd
Levi <anfengmin@yeah.net>
2025-05-30 23:16:38 +0800

    feat: EASTCODE

--- snail-sys/src/main/java/com/snail/sys/service/impl/SysOssServiceImpl.java
+++ snail-sys/src/main/java/com/snail/sys/service/impl/SysOssServiceImpl.java
@@ -2,24 +2,34 @@ package com.snail.sys.service.impl;
 
 import com.snail.sys.domain.SysOss;
 import com.snail.sys.dao.SysOssDao;
+import com.snail.sys.dto.SysOssPageDTO;
 import com.snail.sys.service.SysOssService;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.snail.common.core.utils.R;
 import org.springframework.stereotype.Service;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 
 
-import javax.annotation.Resource;
-
 /**
- * OSS对象存储(SysOss)表服务实现类
+ * OSS对象存储
  *
  * @author makejava
- * @since 2025-05-21 21:52:20
+ * @since 2025-05-30 23:04:33
  */
 @Service("sysOssService")
 public class SysOssServiceImpl extends ServiceImpl<SysOssDao, SysOss> implements SysOssService {
 
-    @Resource
-    private SysOssDao sysOssDao;
 
-
+    /**
+     * 分页查询
+     *
+     * @param dto 筛选条件
+     * @return 查询结果
+     */
+    @Override
+    public R<Page<SysOss>> queryByPage(SysOssPageDTO dto) {
+        Page<SysOss> page = new Page<>(dto.getCurrent(), dto.getSize());
+        Page<SysOss> result = this.lambdaQuery().page(page);
+        return R.ok(result);
+    }
 }
