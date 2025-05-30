130a14bf410068ca7d91a1dec73fa3843bbaa113
Levi <anfengmin@yeah.net>
2025-05-30 23:17:29 +0800

    feat: EASTCODE

--- snail-sys/src/main/java/com/snail/sys/service/SysRoleDeptService.java
+++ snail-sys/src/main/java/com/snail/sys/service/SysRoleDeptService.java
@@ -1,16 +1,27 @@
 package com.snail.sys.service;
 
 import com.snail.sys.domain.SysRoleDept;
+import com.snail.sys.dto.SysRoleDeptPageDTO;
 import com.baomidou.mybatisplus.extension.service.IService;
-
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.snail.common.core.utils.R;
 
 /**
- * 角色和部门关联(SysRoleDept)表服务接口
+ * 角色和部门关联
  *
  * @author makejava
- * @since 2025-05-21 21:53:01
+ * @since 2025-05-30 23:06:26
  */
 public interface SysRoleDeptService extends IService<SysRoleDept> {
+
+    /**
+     * 分页查询
+     *
+     * @param dto dto
+     * @return 分页结果
+     * @since 1.0
+     */
+    R<Page<SysRoleDept>> queryByPage(SysRoleDeptPageDTO dto);
 
 
 }
