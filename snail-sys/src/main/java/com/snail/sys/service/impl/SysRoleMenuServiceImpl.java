0dd6ce0491b3b1f9c1a2079ebaab72050312e507
Levi <anfengmin@yeah.net>
2025-05-30 23:14:50 +0800

    feat: EASTCODE

--- snail-sys/src/main/java/com/snail/sys/service/impl/SysRoleMenuServiceImpl.java
+++ snail-sys/src/main/java/com/snail/sys/service/impl/SysRoleMenuServiceImpl.java
@@ -2,24 +2,34 @@ package com.snail.sys.service.impl;
 
 import com.snail.sys.domain.SysRoleMenu;
 import com.snail.sys.dao.SysRoleMenuDao;
+import com.snail.sys.dto.SysRoleMenuPageDTO;
 import com.snail.sys.service.SysRoleMenuService;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.snail.common.core.utils.R;
 import org.springframework.stereotype.Service;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 
 
-import javax.annotation.Resource;
-
 /**
- * 角色和菜单关联(SysRoleMenu)表服务实现类
+ * 角色和菜单关联
  *
  * @author makejava
- * @since 2025-05-21 21:53:02
+ * @since 2025-05-30 23:06:44
  */
 @Service("sysRoleMenuService")
 public class SysRoleMenuServiceImpl extends ServiceImpl<SysRoleMenuDao, SysRoleMenu> implements SysRoleMenuService {
 
-    @Resource
-    private SysRoleMenuDao sysRoleMenuDao;
 
-
+    /**
+     * 分页查询
+     *
+     * @param dto 筛选条件
+     * @return 查询结果
+     */
+    @Override
+    public R<Page<SysRoleMenu>> queryByPage(SysRoleMenuPageDTO dto) {
+        Page<SysRoleMenu> page = new Page<>(dto.getCurrent(), dto.getSize());
+        Page<SysRoleMenu> result = this.lambdaQuery().page(page);
+        return R.ok(result);
+    }
 }
