17cee646bfbdb826153c9bd6c797b493bcae1a19
Levi <anfengmin@yeah.net>
2025-05-30 23:18:26 +0800

    feat: EASTCODE

--- snail-sys/src/main/java/com/snail/sys/service/SysUserService.java
+++ snail-sys/src/main/java/com/snail/sys/service/SysUserService.java
@@ -1,21 +1,20 @@
 package com.snail.sys.service;
 
-import com.snail.common.core.utils.R;
-import com.snail.sys.api.domain.SysUser;
-import com.snail.common.satoken.vo.LoginUser;
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.snail.sys.dto.SysUserPageDTO;
-import org.springframework.data.domain.Page;
 
+import com.snail.common.satoken.vo.LoginUser;
+import com.snail.sys.api.domain.SysUser;
+import com.snail.sys.dto.SysUserPageDTO;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.snail.common.core.utils.R;
 
 /**
- * 用户信息表(User)表服务接口
+ * 用户
  *
  * @author makejava
- * @since 2025-05-11 20:30:04
+ * @since 2025-05-30 23:06:59
  */
 public interface SysUserService extends IService<SysUser> {
-
 
     /**
      * 分页查询
@@ -30,10 +29,8 @@ public interface SysUserService extends IService<SysUser> {
      * getUserInfo
      *
      * @param userCode userCode
-     * @return com.snail.sys.api.vo.UserVo
-     * @author Anfm(✪ ω ✪)
+     * @return com.snail.common.satoken.vo.LoginUser
      * @since 1.0
-     * <p>1.0 Initialization method </p>
      */
     LoginUser getUserInfo(String userCode);
 
@@ -43,8 +40,8 @@ public interface SysUserService extends IService<SysUser> {
      * @param sysUser sysUser
      * @return boolean
      * @since 1.0
-     * <p>1.0 Initialization method </p>
      */
     boolean registerUserInfo(SysUser sysUser);
+
 
 }
