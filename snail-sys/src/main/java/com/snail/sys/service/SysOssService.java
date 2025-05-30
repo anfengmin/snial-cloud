5add0d310dfb2a3f23f6d35aed0f006ed02bc44b
Levi <anfengmin@yeah.net>
2025-05-30 23:16:41 +0800

    feat: EASTCODE

--- snail-sys/src/main/java/com/snail/sys/service/SysOssService.java
+++ snail-sys/src/main/java/com/snail/sys/service/SysOssService.java
@@ -1,16 +1,27 @@
 package com.snail.sys.service;
 
 import com.snail.sys.domain.SysOss;
+import com.snail.sys.dto.SysOssPageDTO;
 import com.baomidou.mybatisplus.extension.service.IService;
-
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.snail.common.core.utils.R;
 
 /**
- * OSS对象存储(SysOss)表服务接口
+ * OSS对象存储
  *
  * @author makejava
- * @since 2025-05-21 21:52:20
+ * @since 2025-05-30 23:04:29
  */
 public interface SysOssService extends IService<SysOss> {
+
+    /**
+     * 分页查询
+     *
+     * @param dto dto
+     * @return 分页结果
+     * @since 1.0
+     */
+    R<Page<SysOss>> queryByPage(SysOssPageDTO dto);
 
 
 }
