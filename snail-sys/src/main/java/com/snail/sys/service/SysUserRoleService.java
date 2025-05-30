7c1b5bc8808cac1f2df39c28eb802ac16fc85fd9
Levi <anfengmin@yeah.net>
2025-05-30 23:18:20 +0800

    feat: EASTCODE

--- snail-sys/src/main/java/com/snail/sys/service/SysUserRoleService.java
+++ snail-sys/src/main/java/com/snail/sys/service/SysUserRoleService.java
@@ -1,16 +1,27 @@
 package com.snail.sys.service;
 
 import com.snail.sys.domain.SysUserRole;
+import com.snail.sys.dto.SysUserRolePageDTO;
 import com.baomidou.mybatisplus.extension.service.IService;
-
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.snail.common.core.utils.R;
 
 /**
- * 用户和角色关联表(SysUserRole)表服务接口
+ * 用户和角色关联表
  *
  * @author makejava
- * @since 2025-05-21 21:53:04
+ * @since 2025-05-30 23:14:07
  */
 public interface SysUserRoleService extends IService<SysUserRole> {
+
+    /**
+     * 分页查询
+     *
+     * @param dto dto
+     * @return 分页结果
+     * @since 1.0
+     */
+    R<Page<SysUserRole>> queryByPage(SysUserRolePageDTO dto);
 
 
 }
