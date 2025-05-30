04359a415b5bb7f0d25790c47e412cffb22ef6f2
Levi <anfengmin@yeah.net>
2025-05-30 23:14:55 +0800

    feat: EASTCODE

--- snail-sys/src/main/java/com/snail/sys/service/impl/SysPostServiceImpl.java
+++ snail-sys/src/main/java/com/snail/sys/service/impl/SysPostServiceImpl.java
@@ -2,24 +2,34 @@ package com.snail.sys.service.impl;
 
 import com.snail.sys.domain.SysPost;
 import com.snail.sys.dao.SysPostDao;
+import com.snail.sys.dto.SysPostPageDTO;
 import com.snail.sys.service.SysPostService;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.snail.common.core.utils.R;
 import org.springframework.stereotype.Service;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 
 
-import javax.annotation.Resource;
-
 /**
- * 岗位信息(SysPost)表服务实现类
+ * 岗位信息
  *
  * @author makejava
- * @since 2025-05-21 21:53:00
+ * @since 2025-05-30 23:05:47
  */
 @Service("sysPostService")
 public class SysPostServiceImpl extends ServiceImpl<SysPostDao, SysPost> implements SysPostService {
 
-    @Resource
-    private SysPostDao sysPostDao;
 
-
+        /**
+         * 分页查询
+         *
+         * @param dto      筛选条件
+         * @return 查询结果
+         */
+        @Override
+        public R<Page<SysPost>> queryByPage(SysPostPageDTO dto) {
+            Page<SysPost> page = new Page<>(dto.getCurrent(), dto.getSize());
+            Page<SysPost> result = this.lambdaQuery().page(page);
+            return R.ok(result);
+        }
 }
