3b391606bbe72c76d2125dc14b8ca13ad1f976be
Levi <anfengmin@yeah.net>
2025-05-31 21:40:02 +0800

    feat: menu

--- snail-sys/src/main/java/com/snail/sys/service/SysMenuService.java
+++ snail-sys/src/main/java/com/snail/sys/service/SysMenuService.java
@@ -1,10 +1,12 @@
 package com.snail.sys.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.snail.common.core.utils.R;
 import com.snail.sys.domain.SysMenu;
 import com.snail.sys.dto.SysMenuPageDTO;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+
+import java.util.List;
 
 /**
  * 菜单权限
@@ -13,6 +15,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
  * @since 2025-05-29 21:45:00
  */
 public interface SysMenuService extends IService<SysMenu> {
+
 
     /**
      * 分页查询
@@ -23,5 +26,13 @@ public interface SysMenuService extends IService<SysMenu> {
      */
     R<Page<SysMenu>> queryByPage(SysMenuPageDTO dto);
 
-
+    /**
+     * 获取菜单列表
+     *
+     * @param menu   menu
+     * @param userId userId
+     * @return java.util.List<com.snail.sys.domain.SysMenu>
+     * @since 1.0
+     */
+    List<SysMenu> queryMenuList(SysMenu menu, Long userId);
 }
