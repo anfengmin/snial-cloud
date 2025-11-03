77d6e279e51ee6a66876980294a63c01d0c7af75
anfengmin <anfengmin@yeah.net>
2025-11-03 11:14:37 +0800

    feat(gateway): 添加滑动验证码获取功能
    - 实现滑动验证码生成接口
    - 集成验证码服务处理逻辑- 添加异常处理机制
    - 返回标准响应格式数据
    - 支持验证码图片和坐标信息获取
    - 完善接口文档注释说明

---
+++ snail-gateway/src/main/java/com/snail/gateway/handler/SlidingCaptchaHandler.java
@@ -0,0 +1,40 @@
+package com.snail.gateway.handler;
+
+import com.snail.gateway.service.ValidateCodeService;
+import com.snail.gateway.vo.SlidingCaptchaVo;
+import com.snail.common.core.exception.CaptchaException;
+import com.snail.common.core.utils.R;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.HandlerFunction;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+
+/**
+ * 滑动验证码获取
+ *
+ * @author ruoyi
+ */
+@Component
+public class SlidingCaptchaHandler implements HandlerFunction<ServerResponse> {
+
+    @Resource
+    private ValidateCodeService validateCodeService;
+
+    @Override
+    public Mono<ServerResponse> handle(ServerRequest serverRequest) {
+        R<SlidingCaptchaVo> ajax;
+        try {
+            ajax = validateCodeService.createSlidingCaptcha();
+        } catch (CaptchaException | IOException e) {
+            return Mono.error(e);
+        }
+        return ServerResponse.status(HttpStatus.OK).body(BodyInserters.fromValue(ajax));
+    }
+}
+
