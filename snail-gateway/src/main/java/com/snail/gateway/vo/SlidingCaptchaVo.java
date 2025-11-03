5abd3642d049f7006aa10c23efb8611f8f19d0c6
anfengmin <anfengmin@yeah.net>
2025-11-03 11:15:10 +0800

    feat(gateway): 新增滑动验证码返回对象
    - 创建 SlidingCaptchaVo 类用于封装滑动验证码数据- 添加 uuid、backgroundImg 和 sliderImg 字段
    - 使用 Swagger 注解定义 API 模型和属性说明
    - 实现 Serializable 接口以支持序列化- 添加 Lombok @Data 注解简化 getter/setter代码

---
+++ snail-gateway/src/main/java/com/snail/gateway/vo/SlidingCaptchaVo.java
@@ -0,0 +1,29 @@
+package com.snail.gateway.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 滑动验证码返回对象
+ *
+ * @author ruoyi
+ */
+@Data
+@ApiModel(value = "滑动验证码")
+public class SlidingCaptchaVo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "唯一标识")
+    private String uuid;
+
+    @ApiModelProperty(value = "背景图(base64)")
+    private String backgroundImg;
+
+    @ApiModelProperty(value = "滑块图(base64)")
+    private String sliderImg;
+}
+
