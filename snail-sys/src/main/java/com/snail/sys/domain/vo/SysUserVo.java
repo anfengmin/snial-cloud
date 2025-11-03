e11973b47b02dfd9380ad1fc2490269daac9ed42
anfengmin <anfengmin@yeah.net>
2025-11-03 17:51:11 +0800

    feat(sys): 添加SysUserVo实体类- 新增SysUserVo类用于封装用户信息视图对象 - 包含用户基本信息、菜单权限和角色权限字段 - 使用Lombok注解简化代码编写 - 集成Swagger注解提供接口文档支持 - 定义用户权限相关集合类型字段 - 设置完整的类注释和作者信息

---
+++ snail-sys/src/main/java/com/snail/sys/domain/vo/SysUserVo.java
@@ -0,0 +1,29 @@
+package com.snail.sys.domain.vo;
+
+import com.snail.sys.api.domain.SysUser;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.Set;
+
+/**
+ * method
+ *
+ * @author Anfm
+ * Created time 2025/11/3
+ * @since 1.0
+ */
+@Builder
+@Data
+public class SysUserVo {
+
+    @ApiModelProperty(value = "用户信息")
+    private SysUser sysUser;
+
+    @ApiModelProperty(value = "菜单权限")
+    private Set<String> menuPermission;
+
+    @ApiModelProperty(value = "角色权限")
+    private Set<String> rolePermission;
+}
