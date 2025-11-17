2ba1932ca668f08e137ae7cff3f6e317f389ced3
anfengmin <anfengmin@yeah.net>
2025-11-17 09:14:16 +0800

    feat(sys): 1

--- snail-sys/src/main/java/com/snail/sys/domain/SysMenu.java
+++ snail-sys/src/main/java/com/snail/sys/domain/SysMenu.java
@@ -4,10 +4,15 @@ import java.util.Date;
 
 import com.baomidou.mybatisplus.annotation.*;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  * 菜单权限(SysMenu)实体类
@@ -28,18 +33,22 @@ public class SysMenu extends Model<SysMenu> {
     @ApiModelProperty(value = "菜单ID")
     private Long id;
 
+    @NotBlank(message = "菜单名称不能为空")
     @ApiModelProperty(value = "菜单名称")
     private String menuName;
 
     @ApiModelProperty(value = "父菜单ID")
     private Long parentId;
 
+    @NotNull(message = "显示顺序不能为空")
     @ApiModelProperty(value = "显示顺序")
     private Integer orderNum;
 
+    @Size(min = 0, max = 200, message = "路由地址不能超过200个字符")
     @ApiModelProperty(value = "路由地址")
     private String path;
 
+    @Size(min = 0, max = 200, message = "组件路径不能超过255个字符")
     @ApiModelProperty(value = "组件路径")
     private String component;
 
@@ -52,6 +61,7 @@ public class SysMenu extends Model<SysMenu> {
     @ApiModelProperty(value = "是否缓存（0缓存 1不缓存）")
     private Integer isCache;
 
+    @NotBlank(message = "菜单类型不能为空")
     @ApiModelProperty(value = "菜单类型（M目录 C菜单 F按钮）")
     private String menuType;
 
@@ -65,6 +75,8 @@ public class SysMenu extends Model<SysMenu> {
     @ApiModelProperty(value = "删除标志（0:存在 1:删除）")
     private Integer deleted;
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @Size(min = 0, max = 100, message = "权限标识长度不能超过100个字符")
     @ApiModelProperty(value = "权限标识")
     private String perms;
 
