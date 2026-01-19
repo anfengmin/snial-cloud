b3bac1528ea660cce3eaff75824916b41d21f255
anfengmin <anfengmin@yeah.net>
2026-01-19 09:58:05 +0800

    feat(log): 添加系统访问记录分页查询数据传输对象
    - 创建SysLogPageDTO类继承PageBaseEntity
    - 定义访问ID、用户账号、登录IP地址等属性
    - 添加登录地点、浏览器类型、操作系统等字段
    - 包含登录状态、提示消息、访问时间等信息
    - 使用TableName注解映射到sys_login_info表
    - 集成Swagger API文档注解支持

---
+++ snail-sys/src/main/java/com/snail/sys/dto/SysLogPageDTO.java
@@ -0,0 +1,51 @@
+package com.snail.sys.dto;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.snail.sys.api.domain.PageBaseEntity;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.Date;
+
+/**
+ * method
+ *
+ * @author Anfm
+ * Created time 2026/1/19
+ * @since 1.0
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@TableName("sys_login_info")
+@ApiModel(value = "系统访问记录")
+public class SysLogPageDTO extends PageBaseEntity {
+
+    @ApiModelProperty(value = "访问ID")
+    private Long id;
+
+    @ApiModelProperty(value = "用户账号")
+    private String userName;
+
+    @ApiModelProperty(value = "登录IP地址")
+    private String ipaddr;
+
+    @ApiModelProperty(value = "登录地点")
+    private String loginLocation;
+
+    @ApiModelProperty(value = "浏览器类型")
+    private String browser;
+
+    @ApiModelProperty(value = "操作系统")
+    private String os;
+
+    @ApiModelProperty(value = "登录状态（0成功 1失败）")
+    private String status;
+
+    @ApiModelProperty(value = "提示消息")
+    private String msg;
+
+    @ApiModelProperty(value = "访问时间")
+    private Date loginTime;
+}
