b02cfb490c62f394eaa756d7ef14cf6edff2840f
Levi <anfengmin@yeah.net>
2026-02-26 22:17:25 +0800

    feat(sys): 更新岗位服务接口定义
    - 调整导入包顺序，统一按规范排列
    - 修改分页查询方法返回类型为Page对象
    - 新增岗位名称重复检查方法
    - 新增岗位编码重复检查方法
    - 新增岗位列表查询方法
    - 完善方法注释文档

--- snail-sys/src/main/java/com/snail/sys/service/SysPostService.java
+++ snail-sys/src/main/java/com/snail/sys/service/SysPostService.java
@@ -1,10 +1,11 @@
 package com.snail.sys.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
 import com.snail.sys.domain.SysPost;
 import com.snail.sys.dto.SysPostPageDTO;
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.snail.common.core.utils.R;
+
+import java.util.List;
 
 /**
  * 岗位信息
@@ -14,14 +15,38 @@ import com.snail.common.core.utils.R;
  */
 public interface SysPostService extends IService<SysPost> {
 
- /**
+    /**
      * 分页查询
      *
      * @param dto dto
      * @return 分页结果
      * @since 1.0
      */
-     R<Page<SysPost>> queryByPage(SysPostPageDTO dto);
+    Page<SysPost> queryByPage(SysPostPageDTO dto);
 
+    /**
+     * 检查岗位名称是否存在
+     *
+     * @param sysPost sysPost
+     * @return 是否存在
+     * @since 1.0
+     */
+    boolean checkPostNameExists(SysPost sysPost);
 
+    /**
+     * 检查岗位编码是否存在
+     *
+     * @param sysPost sysPost
+     * @return 是否存在
+     * @since 1.0
+     */
+    boolean checkPostCodeExists(SysPost sysPost);
+
+    /**
+     * 查询岗位列表
+     *
+     * @param post 岗位信息
+     * @return 岗位列表
+     */
+    List<SysPost> selectPostList(SysPost post);
 }
