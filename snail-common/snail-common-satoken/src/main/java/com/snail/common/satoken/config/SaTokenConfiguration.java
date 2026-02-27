9b0b15aa9e943023a093cee835a4c1e1490c80a7
Levi <anfengmin@yeah.net>
2026-02-27 23:04:08 +0800

    feat(auth): 实现用户认证和授权功能
    - 添加用户登录、登出、注册和token获取接口
    - 集成Sa-Token-JWT实现JWT令牌管理
    - 添加负载均衡支持和网关路由配置
    - 实现自定义SaTokenDao存储逻辑
    - 配置JWT简单模式和权限验证接口
    - 移除Redis配置类并优化依赖管理

--- snail-common/snail-common-satoken/src/main/java/com/snail/common/satoken/config/SaTokenConfiguration.java
+++ snail-common/snail-common-satoken/src/main/java/com/snail/common/satoken/config/SaTokenConfiguration.java
@@ -1,6 +1,10 @@
 package com.snail.common.satoken.config;
 
+import cn.dev33.satoken.dao.SaTokenDao;
+import cn.dev33.satoken.jwt.StpLogicJwtForSimple;
 import cn.dev33.satoken.stp.StpInterface;
+import cn.dev33.satoken.stp.StpLogic;
+import com.snail.common.satoken.core.dao.PlusSaTokenDao;
 import com.snail.common.satoken.core.service.SaPermissionImpl;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -21,6 +25,11 @@ import org.springframework.context.annotation.Bean;
 @AutoConfiguration
 public class SaTokenConfiguration {
 
+
+    @Bean
+    public StpLogic getStpLogicJwt() {
+        return new StpLogicJwtForSimple();
+    }
     /**
      * 权限接口实现
      * <p>
@@ -32,5 +41,14 @@ public class SaTokenConfiguration {
     @Bean
     public StpInterface stpInterface() {
         return new SaPermissionImpl();
+    }
+
+
+    /**
+     * 自定义dao层存储
+     */
+    @Bean
+    public SaTokenDao saTokenDao() {
+        return new PlusSaTokenDao();
     }
 }
--- snail-sys/src/main/java/com/snail/auth/controller/OauthController.java
+++ snail-sys/src/main/java/com/snail/auth/controller/OauthController.java
@@ -1,18 +1,28 @@
 package com.snail.auth.controller;
 
+import cn.dev33.satoken.stp.SaTokenInfo;
+import cn.dev33.satoken.stp.StpUtil;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.snail.auth.form.LoginBody;
+import com.snail.auth.form.RegisterBody;
+import com.snail.auth.service.SysLoginService;
+import com.snail.common.core.constant.Constants;
 import com.snail.common.core.utils.MessageUtils;
 import com.snail.common.core.utils.R;
+import com.snail.common.satoken.utils.LoginUtils;
+import com.snail.sys.api.domain.LoginUser;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -24,14 +34,58 @@ import java.util.Map;
  */
 @Slf4j
 @Api(tags = "登录")
-@Validated
-@RequestMapping("oauth/v1")
+@RequiredArgsConstructor
+@RequestMapping("/auth")
 @RestController
 public class OauthController {
 
 
+
+    private final SysLoginService sysLoginService;
+
     @ApiModelProperty(value = "登录")
+    @PostMapping("login")
+    @ApiOperation("用户登录")
+    public R<Map<String, Object>> login(@Validated @RequestBody LoginBody form) {
+        // 用户登录
+        String accessToken = sysLoginService.login(form.getUserCode(), form.getPassWord());
+        // 接口返回信息
+        Map<String, Object> rspMap = new HashMap<>();
+        String token = Constants.BEARER + accessToken;
+        rspMap.put(Constants.ACCESS_TOKEN, token);
+        return R.ok(rspMap);
+    }
+
+    @ApiModelProperty(value = "登出")
+    @PostMapping("logout")
+    @ApiOperation("用户登出")
+    public R<Void> logout() {
+        sysLoginService.logout();
+        return R.ok();
+    }
+
+    @GetMapping("/isLogin")
+    @ApiOperation("判断用户是否登录")
+    public R<Object> isLogin(@RequestParam("userCode") String userCode) {
+        LoginUser loginUser1 = LoginUtils.getLoginUser();
+        boolean login = StpUtil.isLogin(userCode);
+        SaTokenInfo tokenInfo = StpUtil.getTokenInfo();
+        LoginUser loginUser = LoginUtils.getLoginUser("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJsb2dpblR5cGUiOiJsb2dpbiIsImxvZ2luSWQiOiJzeXNfdXNlcjoxIiwicm5TdHIiOiIxZ3NUdFZTa0F1aUZKZTB6Um5jb2pTdU5wVFdPdHUxVSIsInVzZXJJZCI6MX0.LGZOLO3D-cE31afzXr0lRDfVjAODWsy82h8dWio8dbM");
+        return R.ok(login);
+    }
+
+
+    @PostMapping("register")
+    @ApiOperation("用户注册")
+    public R<Void> register(@RequestBody RegisterBody registerBody) {
+        // 用户注册
+        sysLoginService.register(registerBody);
+        return R.ok();
+    }
+
+
     @PostMapping("token")
+    @ApiOperation("获取token")
     public R<Map<String, Object>> token(@ModelAttribute LoginBody form) {
 
         log.info("登录信息：{}", form);
