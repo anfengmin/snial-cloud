c5785e3cff7df97bbf8bad810e8103067c291645
anfengmin <anfengmin@yeah.net>
2026-02-27 15:26:39 +0800

    feat(sys-role): 添加角色分配用户查询功能
    - 引入 QueryWrapper 和 Page 分页相关依赖
    - 添加 SysUser 和 SysNotice 实体类引用
    - 实现分页查询已分配用户列表功能
    - 新增 selectAllocatedList 方法支持角色权限管理
    - 集成 MyBatis-Plus 注解配置分页查询参数

--- snail-sys/src/main/java/com/snail/sys/dao/SysRoleDao.java
+++ snail-sys/src/main/java/com/snail/sys/dao/SysRoleDao.java
@@ -1,8 +1,14 @@
 package com.snail.sys.dao;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.snail.sys.api.domain.SysUser;
+import com.snail.sys.domain.SysNotice;
 import com.snail.sys.domain.SysRole;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 
 /**
@@ -14,5 +20,13 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface SysRoleDao extends BaseMapper<SysRole> {
 
+    /**
+     * 查询指定行数据
+     *
+     * @param page      分页参数
+     * @param wrapper   查询条件
+     * @return 对象列表
+     */
+    Page<SysUser> selectAllocatedList(@Param("page") Page<SysNotice> page,@Param(Constants.WRAPPER) QueryWrapper<SysUser> wrapper);
 }
 
