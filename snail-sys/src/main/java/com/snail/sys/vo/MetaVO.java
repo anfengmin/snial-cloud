abb1fa76b097849c45a449506102b26e48f5c9f6
anfengmin <anfengmin@yeah.net>
2026-03-05 17:06:26 +0800

    feat(router): 添加路由显示信息数据传输对象
    - 创建MetaVO类用于封装路由元数据信息
    - 添加标题、国际化键值、图标等路由显示属性
    - 实现路由缓存、固定路由、隐藏菜单等功能标识
    - 集成Swagger文档注解便于API文档生成
    - 使用Lombok注解简化getter/setter方法代码
    - 配置JSON序列化策略过滤空值字段

---
+++ snail-sys/src/main/java/com/snail/sys/vo/MetaVO.java
@@ -0,0 +1,48 @@
+package com.snail.sys.vo;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 路由显示信息
+ *
+ * 本类放在 snail-sys 模块中，供系统服务内部使用。
+ *
+ * @author Anfm
+ * @since 1.0
+ */
+@ApiModel("路由显示信息")
+@Data
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class MetaVO {
+
+    @ApiModelProperty("设置该路由在侧边栏和面包屑中展示的名字")
+    private String title;
+
+    @JsonInclude(JsonInclude.Include.ALWAYS)
+    @ApiModelProperty("设置该路由的i18nKey")
+    private String i18nKey;
+
+    /**
+     * 设置该路由的图标，对应路径src/assets/icons/svg
+     */
+    @ApiModelProperty("设置该路由的图标")
+    private String icon;
+
+    @ApiModelProperty("是否缓存该路由")
+    private boolean keepAlive;
+
+    @ApiModelProperty("动态路由固定路由")
+    private boolean constant;
+
+    @ApiModelProperty("是否隐藏路由")
+    private boolean hideInMenu;
+
+    @ApiModelProperty("内链地址")
+    private String link;
+}
+
